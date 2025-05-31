a83e805e49d0a0526efa7ddf7c290e701db4cd4d
steins0668 <archelausagaton@gmail.com>
2025-05-31 15:31:47 +0800

    add cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+import cors from 'cors';
 import express from 'express';
-import router from "./routes/projectsRoute.js";
+import { router as projectsRouter } from "./routes/projectsRoute.js";
 import { turso } from './db/csods.js';
 import { ROUTES } from './constants/constants.js';
 
@@ -10,9 +11,11 @@ const app = express()
 
 //  for express json parsing
 app.use(express.json());
+//  whitelist api so connection works and you can make requests.
+app.use(cors());
 
 //  for routes
-app.use(ROUTES.PROJECTS, router);
+app.use(ROUTES.PROJECTS, projectsRouter);
 
 app.listen(3001, () => {
     console.log("Server running on port 3001")
