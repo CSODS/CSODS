fc959d83f2d6e0c6cccf3a9f37f588a232f6bc28
Paul <forgames.app2022@gmail.com>
2025-06-08 05:27:09 +0800

    done db nodels

---
+++ server/db/models/Projects.ts
@@ -0,0 +1,15 @@
+import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
+import { Users } from "./users";
+import { DevTypes } from "./DevTypes";
+
+export const Projects  = sqliteTable("Projects", {
+    ProjectId : integer('ProjectId').primaryKey({autoIncrement: true}).unique(),
+    ProjectNumber : text('ProjectNumber').unique(),
+    UserId : integer('UserId').references(() => Users.UserId, 
+    {onDelete: 'cascade'}).notNull(),
+    ProjectTitle : text('ProjectTitle'),
+    DevTypeId : integer('DevTypeId').references(() => DevTypes.DevTypeId, 
+    {onDelete: 'cascade'}),
+    GitHubUrl : text('GitHubUrl')
+    
+})
