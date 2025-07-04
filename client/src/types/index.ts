736c77ca80b32f61833adf5af267a1064fb41a4d
steins0668 <archelausagaton@gmail.com>
2025-07-04 19:49:19 +0800

    feat(types): add OnHover type and update index export

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,1 +1,1 @@
-export type { Opacity, Color } from './themes/styles';
+export type { Opacity, Color, OnHover } from './themes/styles';
--- client/src/types/themes/styles.ts
+++ client/src/types/themes/styles.ts
@@ -4,3 +4,5 @@ export type LightColor = 'light-1' | 'light-2' | 'light-3';
 export type NeutralColor = 'neutral-1' | 'neutral-2';
 export type DarkColor = 'dark-1' | 'dark-2' | 'dark-3' | 'dark-4';
 export type Color =  LightColor | NeutralColor | DarkColor;
+
+export type OnHover = 'invert' | 'lighten' | 'darken';
