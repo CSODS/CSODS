44a320b7ddcc4158e10843d627f51daf73d0c433
steins0668 <archelausagaton@gmail.com>
2025-07-05 17:36:58 +0800

    refactor(project-list): refactor ProjectList.tsx for better readability. - utilize custom-defined hooks

--- client/src/features/project-list/components/ProjectList.tsx
+++ client/src/features/project-list/components/ProjectList.tsx
@@ -1,45 +1,16 @@
+import { useFetchProjectsPage, useFetchTagData } from "@/hooks";
 import { ProjectDetailsProvider, ProjectDataServiceProvider } from "@/components/shared/Providers";
-import { DEFAULTS } from "@/constants";
-import { IAllProjectTags, IProjectDetails } from "@/types";
-import ApiHandler from "@/utils/api/ApiHandler";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { ProjectCard } from "./ProjectCards";
 import Paginator from "./Paginator";
 
 export function ProjectList () {
-    const [projectList, setProjectList] = useState<IProjectDetails[]>(DEFAULTS.PROJECT_LIST);
-    const [totalPages, setTotalPages] = useState<number>(1);
-    const [allTags, setAllTags] = useState<IAllProjectTags>(DEFAULTS.TAGS);
+    const projectsPage = useFetchProjectsPage();
+    const allTags = useFetchTagData();
 
-    const { pageNumber } = useParams();
-
-    useEffect(() => {
-        const apiHandler = new ApiHandler();
-        const loadProjectList = async() => {
-          if (pageNumber){
-            const projectsPage = await apiHandler.GetProjectsPage(pageNumber);
-            if (projectsPage && projectsPage.Projects) {
-              setTotalPages(projectsPage.TotalPages);
-              setProjectList(projectsPage.Projects);
-            }
-          }
-        }
-        loadProjectList();
-    }, [pageNumber]);
-    
-    useEffect(() => {
-        const apiHandler = new ApiHandler();
-        const loadTagData = async() => {
-          const tagData = await apiHandler.GetAllTags();
-          if (tagData) {
-            setAllTags(tagData);
-          }
-        }
-        loadTagData();
-    }, []);
-
-    if (projectList) {
+    if (projectsPage && allTags) {
+        const projectList = projectsPage.Projects;
+        const totalPages = projectsPage.TotalPages;
+        
         return (
             <div className="row">
                 <div className='mx-0 px-3 mt-5 row row-cols-1 row-cols-md-2 row-gap-4 d-flex justify-content-center'>
@@ -64,6 +35,6 @@ export function ProjectList () {
         );
     }
     else {
-        return <div> no data </div>
+        return <div></div>
     }
 }
