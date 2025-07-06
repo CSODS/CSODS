aca7cbe581a6f9378aa0dac45acf768e4f3ae6d2
steins0668 <archelausagaton@gmail.com>
2025-07-06 23:11:16 +0800

    feat(types): add spacing types for css themes.

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,3 +1,4 @@
+export type { spacing, spacingSettings, marginSettings, paddingSettings } from './themes/spacing';
 export type { IUser } from './api/csods/user/user';
 export type { IProjectDetails, IProject } from './api/csods/projects/project';
 export type { IAllProjectTags, IDevType, IProgrammingLanguage, IFramework, IProjectFramework, IDatabaseTechnology, IApplicationIndustry } from './api/csods/projects/tags/projectTags';
