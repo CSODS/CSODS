51ea7bf8e90c465182240ffae56ef1dbc9f11460
steins0668 <archelausagaton@gmail.com>
2025-07-06 15:50:30 +0800

    refactor(project-list): adjust styles of github and view buttons in project cards

--- client/src/features/project-list/components/ProjectCards.tsx
+++ client/src/features/project-list/components/ProjectCards.tsx
@@ -40,11 +40,11 @@ export default function ProjectCard () {
                 <TagRow tagList={tagList}/>
                 {/* github link and view link */}
                 <div className='mt-3 mb-0 ps-0 pe-0 d-flex flex-row align-items-center'>
-                  <button type='button' className='col-lg-3 px-4 py-2 ms-0 me-3 btn btn-light-1 hover-invert rounded-4 border border-1 border-dark-3' onClick={() => redirectToUrl(projectDetails.Project.GitHubUrl)}>
+                  <button type='button' className='col-lg-3 px-4 py-2 ms-0 me-3 btn-dark-1 hover-lighten rounded-4 border border-0' onClick={() => redirectToUrl(projectDetails.Project.GitHubUrl)}>
                       {/* <img src={github_logo} alt='...' className='img-fluid'/> */}
                       GitHub
                   </button>
-                  <button type='button' className='col-lg-3 px-4 py-2 ms-0 me-3 btn btn-dark-3 hover-invert rounded-4 border border-1 border-light-1' onClick={() => viewProject()}>View</button>
+                  <button type='button' className='col-lg-3 px-4 py-2 ms-0 me-3 btn-dark-1 hover-lighten rounded-4 border border-0' onClick={() => viewProject()}>View</button>
                 </div>
               </div>
             </div>
