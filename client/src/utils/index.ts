50cc70db0722d70dc97ec87091597e2714233281
steins0668 <archelausagaton@gmail.com>
2025-07-06 23:12:59 +0800

    feat(utils): add getBootstrapSpacing method to seamlessly convert component props to css classes

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -2,12 +2,14 @@ import ApiHandler from "./api/ApiHandler";
 import { ProjectDataService, IProjectTags } from "./data/ProjectDataService";
 import { JsonHandler } from "./json/jsonHandler";
 import { redirectToUrl } from "./navigation/navigation";
+import { getBootstrapSpacing } from "./styles/spacing";
 
 export {
     ApiHandler,
     ProjectDataService,
     JsonHandler,
-    redirectToUrl
+    redirectToUrl,
+    getBootstrapSpacing
 };
 
 export type { IProjectTags };
