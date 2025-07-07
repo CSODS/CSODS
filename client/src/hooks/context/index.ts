92448d6b56a4e56628027621582aa606a4b5de93
steins0668 <archelausagaton@gmail.com>
2025-07-08 00:19:32 +0800

    feat(hooks): add custom hook for retrieving TagCategoryContext.

--- client/src/hooks/context/index.ts
+++ client/src/hooks/context/index.ts
@@ -1,3 +1,3 @@
 export { useUser } from "./user";
 export { useProjectDataService } from "./service";
-export { useProjectDetails, useProjectList, useAllTags } from "./project";
+export { useProjectDetails, useProjectList, useTagCategoryMap, useAllTags } from "./project";
