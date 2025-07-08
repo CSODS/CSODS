91a2deb7b50ecf581de302902dd38ebd1d74fc9a
steins0668 <archelausagaton@gmail.com>
2025-07-09 00:06:53 +0800

    feat(project-list): implement functional and dynamic search filters to SearchBar component. - add useState hook for searchFilter and setSearchFilter. - rename handleChange method to handleType for better readability. - add handleRatioBtnChange method for setting the state of searchFilter based on React change events. - include filters when generating page links on pressing enter. - integrate FilterButtons component, replacing old hard-coded variation.

--- client/src/features/project-list/components/SearchBar.tsx
+++ client/src/features/project-list/components/SearchBar.tsx
@@ -6,16 +6,32 @@ import { IProjectSearchParameters } from "@/types";
 import { getPageLink } from "../utils";
 
 export default function SearchBar() {
+  const tagCategoryMap = useTagCategoryMap();
   const [searchString, setSearchString] = useState<string>('');
+  const [searchFilter, setSearchFilter] = useState<string>('');
 
-  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+  const handleType = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setSearchString(evt.target.value);
+  }
+
+  const handleRadioBtnChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchFilter(evt.target.value);
   }
 
   const navigate = useNavigate();
   const handleKeyDown = (evt: React.KeyboardEvent<HTMLInputElement>) => {
     if (evt.key === 'Enter') {
-      const link = getPageLink(1, { projectTitle: searchString });
+      let searchParameters: IProjectSearchParameters = {};
+      const tagDetails = tagCategoryMap.get(searchFilter);
+      if (tagDetails) {
+        const tagCategory = tagDetails.tagCategory;
+        const tagId = tagDetails.tagId;
+        searchParameters = { [tagCategory]: tagId };
+      }
+      if (searchString.trim() !== '') {
+        searchParameters.projectTitle = searchString;
+      }
+      const link = getPageLink(1, searchParameters);
       navigate(link);
       return;
     }
@@ -26,16 +42,17 @@ export default function SearchBar() {
       <div className='mt-5 d-flex justify-content-center align-items-center'>
         <div className='py-3 px-4 col-md-6 d-flex align-items-center justify-content-start border border-light-1 rounded-pill bg-light-3 translucent-20'>
           <i className="bi bi-search color-light-1 me-3"></i>
-          <input type='text' className='m-0 fs-6 w-100 color-light-1' placeholder='Search projects...' value={searchString} onChange={handleChange} onKeyDown={handleKeyDown}/>
+          <input type='text' className='m-0 fs-6 w-100 color-light-1' placeholder='Search projects...' value={searchString} onChange={handleType} onKeyDown={handleKeyDown}/>
         </div>
       </div>
-      <div className='mt-4'>
+      <FilterButtons onRadioBtnChange={handleRadioBtnChange}/>
+      {/* <div className='mt-4'>
         <button type='button' className='btn btn-dark-3 hover-invert translucent-60 rounded-pill my-1 mx-2 px-4 border border-1 border-light-1'>All</button>
         <button type='button' className='btn btn-dark-3 hover-invert translucent-60 rounded-pill my-1 mx-2 px-4 border border-1 border-light-1'>JavaScript</button>
         <button type='button' className='btn btn-dark-3 hover-invert translucent-60 rounded-pill my-1 mx-2 px-4 border border-1 border-light-1'>Python</button>
         <button type='button' className='btn btn-dark-3 hover-invert translucent-60 rounded-pill my-1 mx-2 px-4 border border-1 border-light-1'>C#</button>
         <button type='button' className='btn btn-dark-3 hover-invert translucent-60 rounded-pill my-1 mx-2 px-4 border border-1 border-light-1'>C</button>
-      </div>
+      </div> */}
     </div>
   );
 }
