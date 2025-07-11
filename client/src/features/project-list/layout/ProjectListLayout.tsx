7b9e72a7d81baa0be97d5d350dc5dca6ef66782e
steins0668 <archelausagaton@gmail.com>
2025-07-11 22:53:33 +0800

    refactor(project-list): reduce searchbar height

--- client/src/features/project-list/layout/ProjectListLayout.tsx
+++ client/src/features/project-list/layout/ProjectListLayout.tsx
@@ -83,9 +83,9 @@ function SearchBar() {
   return(
     <div className="ms-1 me-auto col-md-6 col d-lg-none d-flex">
         <div className='w-100 d-flex align-items-center justify-content-center bg-light-1 translucent-30 rounded-1'>
-            <input type='text' className='mx-1 my-2 w-100 fs-6 color-light-1' value={searchString} onChange={handleType} onKeyDown={handleKeyDown}/>
-            <div className="px-3 h-100 d-flex justify-content-center align-items-center bg-dark-3 translucent-70 rounded-end-1">
-                <i className="bi bi-search color-light-1"/>
+            <input type='text' className='mx-1 my-1 w-100 fs-6 color-light-1' value={searchString} onChange={handleType} onKeyDown={handleKeyDown}/>
+            <div className="px-2 h-100 d-flex justify-content-center align-items-center bg-dark-3 translucent-70 rounded-end-1">
+                <i className="px-1 bi bi-search color-light-1"/>
             </div>
         </div>
     </div>
