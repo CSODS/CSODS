5293ab5430cbd6dc406250db21b868edefd79cd2
steins0668 <archelausagaton@gmail.com>
2025-07-13 17:53:10 +0800

    refactor(project-details): adjust child component spacing and font-sizing in Contributors component

--- client/src/features/project-details/components/CoreContributors.tsx
+++ client/src/features/project-details/components/CoreContributors.tsx
@@ -23,7 +23,7 @@ export default function Contributors() {
     ];
 
     return (
-        <div className="py-xl-4 py-lg-3 py-2 card card-frost-gradient-1 hover-shadow border-0 row-gap-3">
+        <div className="py-xl-4 px-md-2 py-lg-3 py-2 card card-frost-gradient-1 hover-shadow border-0 row-gap-3">
             {
                 contributors.map(({Name, Email, Roles}, index) => {
                     return <ContributorRow key={`contributor-${index}`} contributorName={Name} contributorEmail={Email} contributorRoles={Roles}/>
@@ -41,24 +41,24 @@ interface ContributorRowProps {
 
 function ContributorRow({ contributorName, contributorEmail, contributorRoles }: ContributorRowProps) {
     return (
-        <div className="row mx-lg-4 mx-3 me-xl-5 pe-xl-5">
+        <div className="row mx-lg-4 mx-3">
             <div className="col-auto p-0">
                 <i className="bi bi-person-circle color-frost-midnight fs-4"/>
             </div>
-            <div className="col-auto ms-2 me-auto p-0">
+            <div className="col-5 ms-2 me-auto p-0">
                 <p className="m-0 contributor-card-text text-start fw-bold color-frost-light-azure">
                     {contributorName}
                 </p>
-                <p className="m-0 contributor-card-text text-start color-frost-pastel-gray-azure">
+                <p className="m-0 fs-small text-start color-frost-pastel-gray-azure">
                     {contributorEmail}
                 </p>
             </div>
-            <div className="col-auto p-0">
+            <div className="col-5 p-0 pe-1">
                 {
                     contributorRoles.map((role, index) => {
                         return (
                             <div key={`role-${index}`} className="row">
-                                <p className="m-0 contributor-card-text text-center color-frost-light-azure">
+                                <p className="m-0 contributor-card-text text-start color-frost-light-azure">
                                     {role}
                                 </p>
                             </div>
