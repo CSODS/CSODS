0c15c8c020bf61eb635a289c4bc2c162d7399a3a
steins0668 <archelausagaton@gmail.com>
2025-07-14 20:36:46 +0800

    Navbar/tweaks (#37)
    * refactor(project-list): changed anchor tag to NavLink in ProjectListLayout
    
    * refactor(project-list): replace navbar-brand css class with text-decoration-none for the navbar brand

--- client/src/features/project-list/layout/ProjectListLayout.tsx
+++ client/src/features/project-list/layout/ProjectListLayout.tsx
@@ -27,12 +27,12 @@ function NavBar({ children }: NavBarProps) {
     return (
         <div className="p-0 navbar navbar-expand-lg">    
             <div className="px-md-3 px-1 py-0 container-fluid bg-dark-3 translucent-100">
-                <a className="me-md-1 mx-0 navbar-brand d-flex flex-row justify-content-center align-items-center color-light-1" href={ADDRESSES.LANDING_PAGE}>
+                <NavLink className=" m-0 me-md-1 my-1 d-flex flex-row justify-content-center align-items-center text-decoration-none color-light-1" to={ADDRESSES.LANDING_PAGE}>
                     <img src='/lucso-logo-no-bg.png' alt="CSO_LOGO" className='header-logo'/>
                     <p className='m-0 p-0 ps-2 d-lg-block d-none fs-4 bolder'>
                         CSO:DS
                     </p>  
-                </a>
+                </NavLink>
                 {children}
                 <button className="mx-1 p-1 navbar-toggler bg-transparent border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                     <i className="bi bi-list fs-1 color-light-1"></i>
@@ -81,7 +81,7 @@ function SearchBar() {
     }
 
   return(
-    <div className="ms-1 me-auto col-md-6 col d-lg-none d-flex">
+    <div className="ms-1 me-auto col-md-5 col-sm-6 col d-lg-none d-flex">
         <div className='w-100 d-flex align-items-center justify-content-center bg-light-1 translucent-30 rounded-1'>
             <input type='text' className='mx-1 my-1 w-100 fs-6 color-light-1' value={searchString} onChange={handleType} onKeyDown={handleKeyDown}/>
             <div className="px-2 h-100 d-flex justify-content-center align-items-center bg-dark-3 translucent-70 rounded-end-1">
