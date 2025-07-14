4868107ca75b9b113fbea969f6fe529579a20da5
steins0668 <archelausagaton@gmail.com>
2025-07-14 22:09:14 +0800

    feat(hooks): introduce new hooks for memoized project tags and project tag list.

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,3 +1,24 @@
-export { useProjectDetails, useProjectList, useProjectDataService, useTagCategoryMap, useUser, useAllTags } from "./context";
-export { useFetchProjectsPage, useFetchProject, useFetchTagData, useNavigateWithTag, useProjectIcon } from "./project";
-export { usePageNumber, useProjectId, useProjectSearchParams } from './params/useProjectParams';
+export { 
+    useProjectDetails,
+    useProjectList,
+    useProjectDataService,
+    useTagCategoryMap,
+    useUser,
+    useAllTags
+} from "./context";
+
+export {
+    useFetchProjectsPage,
+    useFetchProject,
+    useFetchTagData,
+    useNavigateWithTag,
+    useProjectIcon,
+    useProjectTagList,
+    useProjectTags
+} from "./project";
+
+export {
+    usePageNumber, 
+    useProjectId,
+    useProjectSearchParams
+} from './params/useProjectParams';
