8658f2303b999a55886168618983bec50311521d
steins0668 <archelausagaton@gmail.com>
2025-07-19 23:35:04 +0800

    refactor(middleware): store req method and originalUrl in constants instead of calling them directly in the RouteLogger.
    - better guarding against mutation of the method and originalUrl after the request is finished and the response is sent.

--- server/src/middleware/logger.ts
+++ server/src/middleware/logger.ts
@@ -2,12 +2,15 @@ import { RouteLogger } from "@/utils";
 import { Request, Response, NextFunction } from "express";
 
 export function routeLogger(req: Request, res: Response, next: NextFunction) {
-    RouteLogger.info(`[${req.originalUrl}] Processing request...`);
+    const reqMethod = req.method;
+    const originalUrl = req.originalUrl;
+
+    RouteLogger.info(`[${reqMethod} ${originalUrl}] Processing request...`);
     
     const profiler = RouteLogger.startTimer();
 
     res.on('finish', () => {
-        profiler.done({ message: `[${req.method} ${req.originalUrl}] completed with status ${res.statusCode}.`});
+        profiler.done({ message: `[${reqMethod} ${originalUrl}] completed with status ${res.statusCode}.`});
     })
 
     next();
