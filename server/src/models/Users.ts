e7e82b481d8bbd406e7e1322785d4956b1a2995b
steins0668 <archelausagaton@gmail.com>
2025-07-22 17:14:55 +0800

    refactor(migrations): remove roleId foreign key column from users table

--- server/src/models/Users.ts
+++ server/src/models/Users.ts
@@ -1,5 +1,4 @@
 import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
-import { Roles } from "./Roles.js";
 
 export const Users = sqliteTable("Users", {
   UserId: integer("UserId").unique().primaryKey({autoIncrement: true}),
@@ -7,7 +6,6 @@ export const Users = sqliteTable("Users", {
   Username: text("Username").unique().notNull(),
   StudentName: text("StudentName").notNull(),
   Password: text("Password").notNull(),
-  RoleId: integer("RoleId").references(()=> Roles.RoleId, {onDelete: 'restrict'} ).notNull(),   
   StudentNumber : text("StudentNumber").unique().notNull(),
   UserIconUrl : text('UserIconImg').notNull()
 });
