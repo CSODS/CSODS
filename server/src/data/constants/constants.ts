6cf75536ffcadbe9c7427e65d64643805ca64723
steins0668 <archelausagaton@gmail.com>
2025-07-23 21:14:55 +0800

    refactor(constants): export everything as const

--- server/src/data/constants/constants.ts
+++ server/src/data/constants/constants.ts
@@ -1,53 +1,53 @@
 export const BASE = "http://localhost:3001";
 
 export const ROUTES = {
-    PROJECTS: '/projects',
-    PROJECT_TAGS: '/project_tags',
-    AUTH: '/auth'
+  PROJECTS: "/projects",
+  PROJECT_TAGS: "/project_tags",
+  AUTH: "/auth",
 } as const;
 
 export const PROJECT_ROUTES = {
-    ROOT: '/',
-    BY_PAGE: '/:pageNumber',
-    BY_ID: '/:pageNumber/:projectId',
-    TITLE_SEARCH: '/:title_search',
-    LOAD_PROJECTS: '/load_projects'
+  ROOT: "/",
+  BY_PAGE: "/:pageNumber",
+  BY_ID: "/:pageNumber/:projectId",
+  TITLE_SEARCH: "/:title_search",
+  LOAD_PROJECTS: "/load_projects",
 } as const;
 
 export const PROJECT_TAG_ROUTES = {
-    ROOT: '/',
-    ALL_DATA: '/all_data'
-}
+  ROOT: "/",
+  ALL_DATA: "/all_data",
+} as const;
 
 export const AUTH_ROUTES = {
-    ROOT: '/',
-    SIGN_IN: '/sign-in',
-    REGISTER: '/register',
-    REFRESH: '/refresh',
-    SIGN_OUT: '/sign-out'
-}
+  ROOT: "/",
+  SIGN_IN: "/sign-in",
+  REGISTER: "/register",
+  REFRESH: "/refresh",
+  SIGN_OUT: "/sign-out",
+} as const;
 
 export const CACHE = {
-    BASE_NAME: 'PROJECT_CACHE',
-    HARD_BACKUP: 'PROJECT_CACHE_HARD_BACKUP',
-    AS_JSON: '.json',
-    PAGE_SIZE: 8,
-    TAGS_CACHE: 'TAG_CACHE'
+  BASE_NAME: "PROJECT_CACHE",
+  HARD_BACKUP: "PROJECT_CACHE_HARD_BACKUP",
+  AS_JSON: ".json",
+  PAGE_SIZE: 8,
+  TAGS_CACHE: "TAG_CACHE",
 } as const;
 
 export const DEVELOPMENT_TYPES = {
-    GAME_DEV: "Game Development",
-    WEB_DEV: "Web Development",
-    SOFTWARE_DEV: "Software Development",
-    API_DEV: "API Development"  
+  GAME_DEV: "Game Development",
+  WEB_DEV: "Web Development",
+  SOFTWARE_DEV: "Software Development",
+  API_DEV: "API Development",
 } as const;
 
 export const PROGRAMMING_LANGUAGES = {
-    C_SHARP: 'C#',
-    C: 'C',
-    CPP: 'C++',
-    JAVA: 'Java',
-    JAVASCRIPT: 'Javascript',
-    PYTHON: 'Python',
-    RUBY: 'Ruby'
+  C_SHARP: "C#",
+  C: "C",
+  CPP: "C++",
+  JAVA: "Java",
+  JAVASCRIPT: "Javascript",
+  PYTHON: "Python",
+  RUBY: "Ruby",
 } as const;
