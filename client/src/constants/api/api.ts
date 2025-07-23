c18cb00624d11ce6da9c4507c0e35cc59a6dd775
steins0668 <archelausagaton@gmail.com>
2025-07-24 00:50:41 +0800

    refactor(routes): refactored all routes to use kebab-case

--- client/src/constants/api/api.ts
+++ client/src/constants/api/api.ts
@@ -1,34 +1,33 @@
-const CSODS_API_BASE = (
-    process.env.REACT_APP_ENV === 'testing'
+const CSODS_API_BASE =
+  process.env.REACT_APP_ENV === "testing"
     ? process.env.REACT_APP_CSODS_LOCAL_API_BASE_URL
-    : process.env.REACT_APP_CSODS_API_BASE_URL
-);
+    : process.env.REACT_APP_CSODS_API_BASE_URL;
 
 const PROJECT_PATHS = {
-    PATH: '/projects',
-    ROOT: '/',
-    BY_PAGE: '/:pageNumber',
-    BY_ID: '/:pageNumber/:projectId'
+  PATH: "/projects",
+  ROOT: "/",
+  BY_PAGE: "/:pageNumber",
+  BY_ID: "/:pageNumber/:projectId",
 } as const;
 
 const PROJECT_TAG_PATHS = {
-    PATH: '/project_tags',
-    ROOT: '/',
-    ALL_DATA: '/all_data'
+  PATH: "/project-tags",
+  ROOT: "/",
+  ALL_DATA: "/all-data",
 } as const;
 
 const AUTH_PATHS = {
-    PATH: '/auth',
-    ROOT: '/',
-    SIGN_IN: '/sign-in',
-    REGISTER: '/register',
-    REFRESH: '/refresh',
-    SIGN_OUT: '/sign-out'
+  PATH: "/auth",
+  ROOT: "/",
+  SIGN_IN: "/sign-in",
+  REGISTER: "/register",
+  REFRESH: "/refresh",
+  SIGN_OUT: "/sign-out",
 } as const;
 
 export const CSODS_API_PATHS = {
-    BASE: CSODS_API_BASE,
-    PROJECTS: PROJECT_PATHS,
-    PROJECT_TAGS: PROJECT_TAG_PATHS,
-    AUTH: AUTH_PATHS
+  BASE: CSODS_API_BASE,
+  PROJECTS: PROJECT_PATHS,
+  PROJECT_TAGS: PROJECT_TAG_PATHS,
+  AUTH: AUTH_PATHS,
 } as const;
