f945ed6c8e0f013e2f63e5c45b6730fae759e0be
steins0668 <archelausagaton@gmail.com>
2025-07-23 16:42:13 +0800

    Refactor/server/migrations-modify-users-table (#67)
    * refactor(db): removed notNull modifier of StudentName and StudentNumber columns in Users table
    
    * feat(migrations): add new migration for Users table modification

---
+++ server/migrations/0005_robust_kree.sql
@@ -0,0 +1,19 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_Users` (
+	`UserId` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`Email` text NOT NULL,
+	`Username` text NOT NULL,
+	`StudentName` text,
+	`Password` text NOT NULL,
+	`StudentNumber` text,
+	`UserIconImg` text
+);
+--> statement-breakpoint
+INSERT INTO `__new_Users`("UserId", "Email", "Username", "StudentName", "Password", "StudentNumber", "UserIconImg") SELECT "UserId", "Email", "Username", "StudentName", "Password", "StudentNumber", "UserIconImg" FROM `Users`;--> statement-breakpoint
+DROP TABLE `Users`;--> statement-breakpoint
+ALTER TABLE `__new_Users` RENAME TO `Users`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `Users_UserId_unique` ON `Users` (`UserId`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Users_Email_unique` ON `Users` (`Email`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Users_Username_unique` ON `Users` (`Username`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Users_StudentNumber_unique` ON `Users` (`StudentNumber`);
--- server/src/models/Users.ts
+++ server/src/models/Users.ts
@@ -1,12 +1,17 @@
 import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const Users = sqliteTable("Users", {
-  UserId: integer("UserId").unique().primaryKey({autoIncrement: true}),
-  Email: text("Email").unique().notNull(),
-  Username: text("Username").unique().notNull(),
-  StudentName: text("StudentName").notNull(),
-  Password: text("Password").notNull(),
-  StudentNumber : text("StudentNumber").unique().notNull(),
-  UserIconUrl : text('UserIconImg').notNull()
-});
+  UserId: integer("UserId").unique().primaryKey({ autoIncrement: true }),
 
+  Email: text("Email").unique().notNull(),
+
+  Username: text("Username").unique().notNull(),
+
+  StudentName: text("StudentName"),
+
+  Password: text("Password").notNull(),
+
+  StudentNumber: text("StudentNumber").unique(),
+
+  UserIconUrl: text("UserIconImg"),
+});
