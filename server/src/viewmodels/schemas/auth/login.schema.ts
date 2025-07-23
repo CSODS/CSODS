c1f0b74ad6c971c988fc78ab392462744bf604be
steins0668 <archelausagaton@gmail.com>
2025-07-23 21:58:20 +0800

    feat(auth): add log in schema accepting either an email and password or a username and password

---
+++ server/src/viewmodels/schemas/auth/login.schema.ts
@@ -0,0 +1,42 @@
+import z from "zod";
+import { REGEX } from "@/data";
+
+const AUTH_REGEX = REGEX.AUTH_REGEX;
+
+const loginWithEmail = z.object({
+  Email: z.email({
+    error: (iss) =>
+      iss.input === undefined ? "Email is required." : "Invalid email.",
+  }),
+
+  Password: z
+    .string({
+      error: (iss) =>
+        iss.input === undefined ? "Password is required." : "Invalid Password",
+    })
+    .regex(AUTH_REGEX.PASSWORD, {
+      error: "Invalid password.",
+    }),
+});
+
+const loginWithUsername = z.object({
+  Username: z
+    .string({
+      error: (iss) =>
+        iss.input === undefined ? "Username is required." : "Invalid username.",
+    })
+    .regex(AUTH_REGEX.USERNAME, { error: "Invalid username." }),
+
+  Password: z
+    .string({
+      error: (iss) =>
+        iss.input === undefined ? "Password is required." : "Invalid Password",
+    })
+    .regex(AUTH_REGEX.PASSWORD, {
+      error: "Invalid password.",
+    }),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
+
+export const loginSchema = z.union([loginWithEmail, loginWithUsername]);
