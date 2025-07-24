d0e5bba832c29a50777f9a3d253be20b2471ee02
steins0668 <archelausagaton@gmail.com>
2025-07-24 18:30:32 +0800

    feat(auth): implement verifyRoles middleware to relevant routes

--- server/src/routers/project-tags.router.ts
+++ server/src/routers/project-tags.router.ts
@@ -1,19 +1,29 @@
 import express from "express";
-import { API } from "@data";
-import { validateJWT } from "@/middleware";
+import { API, AUTH } from "@data";
+import { validateJWT, verifyRoles } from "@middleware";
 
 const PROJECT_TAG_ROUTES = API.PROJECT_TAG_ROUTES;
+const { Guest, Student, Moderator, Administrator } = AUTH.ROLES_MAP;
 
 const projectTagsRouter = express.Router();
 
 projectTagsRouter.use(validateJWT);
 
-projectTagsRouter.get(PROJECT_TAG_ROUTES.ALL_DATA, async (req, res) => {
-  const tagsCache = await req.tagsCacheHandler.getTagsCache();
+projectTagsRouter.get(
+  PROJECT_TAG_ROUTES.ALL_DATA,
+  verifyRoles(
+    Guest.roleName,
+    Student.roleName,
+    Moderator.roleName,
+    Administrator.roleName
+  ),
+  async (req, res) => {
+    const tagsCache = await req.tagsCacheHandler.getTagsCache();
 
-  tagsCache
-    ? res.json(tagsCache)
-    : res.status(404).json({ error: "Failed loading tags cache." });
-});
+    tagsCache
+      ? res.json(tagsCache)
+      : res.status(404).json({ error: "Failed loading tags cache." });
+  }
+);
 
 export default projectTagsRouter;
