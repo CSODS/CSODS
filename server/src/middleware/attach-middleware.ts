50c461dd0ceec2f10d2b2870496e2abe3f6c7af9
steins0668 <archelausagaton@gmail.com>
2025-07-24 17:31:19 +0800

    feat(middleware): add optional authPayload to Express Request interface

--- server/src/middleware/attach-middleware.ts
+++ server/src/middleware/attach-middleware.ts
@@ -7,6 +7,7 @@ import {
   UserDataService,
   createUserDataService,
 } from "@services";
+import { TokenPayload } from "@viewmodels";
 
 export function attachTagsCacheHandler(
   req: Request,
@@ -41,6 +42,7 @@ declare global {
       tagsCacheHandler: TagsCacheHandler;
       projectCachePageService: ProjectCachePageService;
       userDataService: UserDataService;
+      authPayload?: TokenPayload;
     }
   }
 }
