27223a4601c0e63dc3b93aeb27773fd04ef2d8f2
steins0668 <archelausagaton@gmail.com>
2025-07-24 18:25:58 +0800

    feat(auth): add ROLES_MAP constant and deprecated ROLES constant

--- server/src/data/constants/auth.constants.ts
+++ server/src/data/constants/auth.constants.ts
@@ -1,9 +1,22 @@
 /**
  * TODO: REPLACE WITH ACTUAL CACHE!!!
+ * @deprecated
  */
 export const ROLES = {
   3: "Guest",
   4: "Student",
   5: "Moderator",
   6: "Administrator",
+} as const;
+
+type RoleName = "Guest" | "Student" | "Moderator" | "Administrator";
+type RoleDetails = { roleId: number; roleName: RoleName };
+
+type RolesMap = Record<RoleName, RoleDetails>;
+
+export const ROLES_MAP: RolesMap = {
+  Guest: { roleId: 3, roleName: "Guest" },
+  Student: { roleId: 4, roleName: "Student" },
+  Moderator: { roleId: 5, roleName: "Moderator" },
+  Administrator: { roleId: 6, roleName: "Administrator" },
 } as const;
