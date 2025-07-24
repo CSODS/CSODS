666efaace117766b378dc9a413932a92061333fe
steins0668 <archelausagaton@gmail.com>
2025-07-24 21:42:03 +0800

    feat(auth): add constant for refresh token cookie name for better modularity

--- server/src/data/constants/index.ts
+++ server/src/data/constants/index.ts
@@ -1,4 +1,5 @@
 export * as API from "./api.constants";
 export * as CACHE from "./cache.constants";
+export * as COOKIES from "./cookies.constants";
 export * as REGEX from "./regex.constants";
 export * as AUTH from "./auth.constants";
