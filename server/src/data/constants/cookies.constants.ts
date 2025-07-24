b84133ab645f5eb512134889f38951b0b598ddf4
steins0668 <archelausagaton@gmail.com>
2025-07-24 23:28:08 +0800

    refactor(auth): delete unused cookie constants

--- server/src/data/constants/cookies.constants.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @deprecated
- * To be replaced with {@link COOKIE_RECORD}
- */
-export const COOKIE_NAMES = {
-  REFRESH_TOKEN: "refresh_token",
-} as const;
--- server/src/data/constants/index.ts
+++ server/src/data/constants/index.ts
@@ -1,5 +1,4 @@
 export * as API from "./api.constants";
-export * as CACHE from "./cache.constants";
-export * as COOKIES from "./cookies.constants";
-export * as REGEX from "./regex.constants";
 export * as AUTH from "./auth.constants";
+export * as CACHE from "./cache.constants";
+export * as REGEX from "./regex.constants";
