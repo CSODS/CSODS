50339573ee528d38fb6ff1120ec145e978500b27
steins0668 <archelausagaton@gmail.com>
2025-07-25 22:42:58 +0800

    refactor(middleware): migrate validateCookies and validateRequest middleware to validation folder

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,5 +1,3 @@
 export * from "./logger";
 export * from "./rate-limiter";
 export * from "./validation";
-export * from "./validate-cookie";
-export * from "./validate-request";
--- server/src/middleware/validation/index.ts
+++ server/src/middleware/validation/index.ts
@@ -1,1 +1,3 @@
+export * from "./validate-cookie";
+export * from "./validate-roles";
 export * from "./validate-roles";
