4169bc617e9a160424580e1083b3df3c41b8622f
steins0668 <archelausagaton@gmail.com>
2025-07-26 22:19:06 +0800

    feat(constants): add AUTH paths to ADDRESSES constants

--- client/src/constants/addresses/addresses.ts
+++ client/src/constants/addresses/addresses.ts
@@ -1,18 +1,27 @@
+const AUTH = {
+  PATH: "/auth",
+  SIGN_IN: "/sign-in",
+  REGISTER: "/register",
+  REFRESH: "/refresh",
+  SIGN_OUT: "/sign-out",
+} as const;
+
 const STUDENT_PROJECTS = {
-    PATH: '/student_projects/:pageNumber',
-    ROOT: '/student_projects'
+  PATH: "/student_projects/:pageNumber",
+  ROOT: "/student_projects",
 } as const;
 
 const PROJECT_DETAILS = {
-    PATH: `${STUDENT_PROJECTS.PATH}/project_details/:projectId`,
-    ROOT: '/project_details'
+  PATH: `${STUDENT_PROJECTS.PATH}/project_details/:projectId`,
+  ROOT: "/project_details",
 } as const;
 
 export const ADDRESSES = {
-    LANDING_PAGE: "/",
-    HOME: "/home",
-    STUDENT_PROJECTS: STUDENT_PROJECTS,
-    PROJECT_DETAILS: PROJECT_DETAILS,
-    SUBMIT_PROJECT: "/submit_project",
-    ABOUT: "/about"
+  AUTH: AUTH,
+  LANDING_PAGE: "/",
+  HOME: "/home",
+  STUDENT_PROJECTS: STUDENT_PROJECTS,
+  PROJECT_DETAILS: PROJECT_DETAILS,
+  SUBMIT_PROJECT: "/submit_project",
+  ABOUT: "/about",
 } as const;
