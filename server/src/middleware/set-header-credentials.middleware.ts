649616c29c9f16d0e26692e8a13fc64f1cbfe637
steins0668 <archelausagaton@gmail.com>
2025-07-27 19:47:34 +0800

    feat(middleware): add set-header-credentials middleware for CORS handling

---
+++ server/src/middleware/set-header-credentials.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { ENV } from "@/data";
+
+export function setHeaderCredentials(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const whitelist = ENV.getCorsWhitelist();
+  const origin = req.headers.origin;
+  if (origin && whitelist.includes(origin)) {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  next();
+}
