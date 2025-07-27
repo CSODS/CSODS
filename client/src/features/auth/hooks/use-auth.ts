38b428d9164ac2ed89d28bf62b756c544f13524b
steins0668 <archelausagaton@gmail.com>
2025-07-27 21:23:09 +0800

    feat(auth): add useAuth hook

---
+++ client/src/features/auth/hooks/use-auth.ts
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../components";
+
+export function useAuth() {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    const errMsg =
+      "Function useAuth must be used inside an AuthProvider component.";
+    throw new Error(errMsg);
+  }
+
+  return authContext;
+}
