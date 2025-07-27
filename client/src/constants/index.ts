48799c27f89baa305d5b2c177283efa0c12e67eb
steins0668 <archelausagaton@gmail.com>
2025-07-27 11:05:59 +0800

    feat(constants): add regex for authenticationk

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,6 +1,11 @@
-export { CSODS_API_PATHS } from './api/api';
+export { CSODS_API_PATHS } from "./api/api";
 export { ADDRESSES } from "./addresses/addresses";
 export { bgConstants } from "./bg-constants";
 export { DEVELOPMENT_TYPE_ENUMS } from "./enums";
-export * as DEFAULTS from './defaults';
-export { DEVELOPMENT_TYPES, ICONS, PROJECT_QUERY_PARAMETERS } from "./project_data/tags";
+export * as DEFAULTS from "./defaults";
+export {
+  DEVELOPMENT_TYPES,
+  ICONS,
+  PROJECT_QUERY_PARAMETERS,
+} from "./project_data/tags";
+export * as REGEX from "./regex.constants";
