ff93018eac8f4cd751ed6729a1f13521a0814751
steins0668 <archelausagaton@gmail.com>
2025-07-27 11:44:07 +0800

    refactor: update login schema to deprecate old types and introduce login options

--- server/src/features/auth/schemas/login.schema.ts
+++ server/src/features/auth/schemas/login.schema.ts
@@ -41,6 +41,29 @@ const loginWithUsername = z.object({
     }),
 });
 
+/**
+ * @deprecated Use {@link LoginOptions}
+ */
 export type LoginSchema = z.infer<typeof loginSchema>;
-
+/**
+ * @deprecated Please use {@link loginOptions}
+ */
 export const loginSchema = z.union([loginWithEmail, loginWithUsername]);
+
+export const loginOptions = z.object({
+  identifier: z.string({
+    error: (iss) => {
+      if (iss.code === "invalid_type") return "Identifier must be a string";
+      if (!iss.input) return "Username or email is required.";
+    },
+  }),
+
+  password: z.string({
+    error: (iss) => {
+      if (iss.code === "invalid_type") return "Password must be a string";
+      if (!iss.input) return "Password is required.";
+    },
+  }),
+});
+
+export type LoginOptions = z.infer<typeof loginOptions>;
