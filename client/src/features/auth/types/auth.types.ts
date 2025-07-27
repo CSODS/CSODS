f9329dce340177949677d36a055e3feded7feb87
steins0668 <archelausagaton@gmail.com>
2025-07-27 14:53:53 +0800

    feat(auth): add type for TokenPayload

--- client/src/features/auth/types/auth.types.ts
+++ client/src/features/auth/types/auth.types.ts
@@ -1,6 +1,15 @@
-export { REGEX } from "@/constants";
-
 export type SignInForm = {
   identifier: string;
   password: string;
+};
+
+export type TokenPayload = {
+  userInfo: {
+    email: string;
+    username: string;
+    studentName: string;
+    studentNumber: string;
+    userIconUrl: string;
+    roles: string;
+  };
 };
