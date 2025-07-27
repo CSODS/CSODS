bf32a939c0429e9c372e1f099164d6f899740f66
steins0668 <archelausagaton@gmail.com>
2025-07-27 14:28:42 +0800

    feat(auth): add mockup signin form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
-import './assets/stylesheets/App.css';
-import './assets/stylesheets/cards.css';
-import './assets/stylesheets/colors.css';
-import './assets/stylesheets/buttons.css';
-import './assets/stylesheets/borders.css';
-import './assets/stylesheets/font.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { ADDRESSES } from '@/constants';
-import { CsodsBackground, NavBar } from '@/components';
-import LandingPage from './features/landing/LandingPage';
+import "./assets/stylesheets/App.css";
+import "./assets/stylesheets/cards.css";
+import "./assets/stylesheets/colors.css";
+import "./assets/stylesheets/buttons.css";
+import "./assets/stylesheets/borders.css";
+import "./assets/stylesheets/font.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ADDRESSES } from "@/constants";
+import { CsodsBackground, NavBar } from "@/components";
+import LandingPage from "./features/landing/LandingPage";
 import Home from "./features/home/Home";
-import StudentProjects from './features/project-list/StudentProjects';
-import ProjectDetails from './features/project-details/ProjectDetails';
-import SubmitProject from './features/submit-project/SubmitProject';
-import { ProjectListLayout } from './features/project-list/layout/ProjectListLayout';
+import StudentProjects from "./features/project-list/StudentProjects";
+import ProjectDetails from "./features/project-details/ProjectDetails";
+import SubmitProject from "./features/submit-project/SubmitProject";
+import { ProjectListLayout } from "./features/project-list/layout/ProjectListLayout";
+import { SignIn } from "./features/auth";
 
 function App() {
   return (
@@ -20,19 +21,30 @@ function App() {
       <Router>
         <Routes>
           <Route path={ADDRESSES.LANDING_PAGE} element={<CsodsBackground />}>
-            <Route element={<NavBar/>}>
+            <Route element={<NavBar />}>
               <Route index element={<LandingPage />} />
+
+              <Route path={ADDRESSES.AUTH.PATH}>
+                <Route path={ADDRESSES.AUTH.SIGN_IN} element={<SignIn />} />
+              </Route>
+
               <Route path={ADDRESSES.HOME} element={<Home />} />
               <Route path={ADDRESSES.ABOUT} />
             </Route>
-            <Route element={<ProjectListLayout navBarVariant={1}/>}>
-              <Route path={ADDRESSES.STUDENT_PROJECTS.PATH} element={<StudentProjects />}/>
+            <Route element={<ProjectListLayout navBarVariant={1} />}>
+              <Route
+                path={ADDRESSES.STUDENT_PROJECTS.PATH}
+                element={<StudentProjects />}
+              />
             </Route>
-            <Route element={<ProjectListLayout navBarVariant={2}/>}>
-              <Route path={ADDRESSES.PROJECT_DETAILS.PATH} element={<ProjectDetails />}/>
+            <Route element={<ProjectListLayout navBarVariant={2} />}>
+              <Route
+                path={ADDRESSES.PROJECT_DETAILS.PATH}
+                element={<ProjectDetails />}
+              />
             </Route>
           </Route>
-          <Route path={ADDRESSES.SUBMIT_PROJECT} element={<SubmitProject />}/>
+          <Route path={ADDRESSES.SUBMIT_PROJECT} element={<SubmitProject />} />
         </Routes>
       </Router>
     </div>
--- client/src/features/auth/SignIn.tsx
+++ client/src/features/auth/SignIn.tsx
@@ -1,6 +1,96 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { authTypes } from "./types";
+import { ADDRESSES } from "@/constants";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export function SignIn() {
-  const [signInForm, setSignInForm] = useState<string>("");
-  return <div></div>;
+  const registerLink = ADDRESSES.AUTH.PATH + "/" + ADDRESSES.AUTH.REGISTER;
+
+  const identifierRef = useRef<HTMLInputElement>(null);
+  const errRef = useRef<HTMLParagraphElement>(null);
+
+  const [errMsg, setErrMsg] = useState<string>("");
+  // todo: replace this with navigating to a different page in the future.
+  const [success, setSuccess] = useState<string>("");
+
+  const [signInForm, setSignInForm] = useState<authTypes.SignInForm>({
+    identifier: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (identifierRef?.current) identifierRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    console.log("Identifier:", signInForm.identifier);
+    console.log("Password:", signInForm.password);
+    setErrMsg("");
+  }, [signInForm.identifier, signInForm.password]);
+
+  const handleType = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const elementName = e.target.id;
+
+    elementName === "identifier"
+      ? setSignInForm((prev) => ({
+          ...prev,
+          identifier: e.target.value,
+        }))
+      : setSignInForm((prev) => ({
+          ...prev,
+          password: e.target.value,
+        }));
+  };
+
+  const navigate = useNavigate();
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log("Identifier:", signInForm.identifier);
+    console.log("Password:", signInForm.password);
+    setSignInForm({ identifier: "", password: "" });
+    e.preventDefault();
+
+    navigate("/home");
+  };
+
+  return (
+    <section>
+      <section className="card-dark-purple border border-1 border-white rounded-3">
+        <article className="p-0">
+          <p
+            ref={errRef}
+            className={`row ${errMsg ? "" : "visually-hidden"}`}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+
+          <h1 className="m-0 p-0 text-center">Welcome</h1>
+
+          <form className="m-0 p-0 row" onSubmit={handleSignIn}>
+            <input
+              type="text"
+              id="identifier"
+              ref={identifierRef}
+              placeholder="Email/Username"
+              className="m-0 p-0 row border"
+              autoComplete="off"
+              onChange={(e) => handleType(e)}
+              value={signInForm.identifier}
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              className="m-0 p-0 row border"
+              onChange={(e) => handleType(e)}
+              value={signInForm.password}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <NavLink to={registerLink}>Create Account</NavLink>
+        </article>
+      </section>
+    </section>
+  );
 }
