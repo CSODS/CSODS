1f82971fbe4049eb988636be0993d1913296a3f9
steins0668 <archelausagaton@gmail.com>
2025-07-28 22:09:23 +0800

    feat(project-list):  move feature-specific hooks and utilities from shared to respective feature folder

--- client/src/features/project-list/components/ProjectList/ProjectList.tsx
+++ client/src/features/project-list/components/ProjectList/ProjectList.tsx
@@ -2,49 +2,45 @@ import {
   ProjectDetailsProvider,
   ProjectDataServiceProvider,
 } from "@/components";
-import { useAllTags, useFetchProjectsPage } from "@/hooks";
-import { TagColorProvider, TotalPagesProvider } from "../Provider";
+import { useAllTags } from "@/hooks";
 import ProjectCard from "./ProjectCard";
 import Paginator from "./Paginator";
+import { TagColorProvider, TotalPagesProvider } from "../Provider";
+import { useFetchProjectsPage } from "./useFetchProjectsPage";
 
 export function ProjectList() {
   const projectsPage = useFetchProjectsPage();
   const allTags = useAllTags();
 
-  if (projectsPage && allTags) {
-    const projectList = projectsPage.Projects;
-    const totalPages = projectsPage.TotalPages;
-
-    return (
-      <div className="container-fluid p-0">
-        <div className="mx-0 mt-md-5 mt-3 px-lg-3 px-sm-1 px-3 row row-cols-1 row-cols-sm-2 row-gap-lg-4 row-gap-2 d-flex justify-content-center">
-          <TagColorProvider allTags={allTags}>
-            <ProjectDataServiceProvider allTags={allTags}>
-              {projectList.map((project, index) => {
-                const cardKey = `project-card-${index}`;
-                return (
-                  <div
-                    key={cardKey}
-                    className="col px-lg-3 px-1"
-                    style={{ maxWidth: 700 }}
-                  >
-                    <ProjectDetailsProvider projectDetails={project}>
-                      <ProjectCard />
-                    </ProjectDetailsProvider>
-                  </div>
-                );
-              })}
-            </ProjectDataServiceProvider>
-          </TagColorProvider>
-        </div>
-        <div className="row m-0">
-          <TotalPagesProvider totalPages={totalPages}>
-            <Paginator />
-          </TotalPagesProvider>
-        </div>
+  return projectsPage && allTags ? (
+    <div className="container-fluid p-0">
+      <div className="mx-0 mt-md-5 mt-3 px-lg-3 px-sm-1 px-3 row row-cols-1 row-cols-sm-2 row-gap-lg-4 row-gap-2 d-flex justify-content-center">
+        <TagColorProvider allTags={allTags}>
+          <ProjectDataServiceProvider allTags={allTags}>
+            {projectsPage.Projects.map((project, index) => {
+              const cardKey = `project-card-${index}`;
+              return (
+                <div
+                  key={cardKey}
+                  className="col px-lg-3 px-1"
+                  style={{ maxWidth: 700 }}
+                >
+                  <ProjectDetailsProvider projectDetails={project}>
+                    <ProjectCard />
+                  </ProjectDetailsProvider>
+                </div>
+              );
+            })}
+          </ProjectDataServiceProvider>
+        </TagColorProvider>
       </div>
-    );
-  } else {
-    return <div></div>;
-  }
+      <div className="row m-0">
+        <TotalPagesProvider totalPages={projectsPage.TotalPages}>
+          <Paginator />
+        </TotalPagesProvider>
+      </div>
+    </div>
+  ) : (
+    <div>Under maintenance. Please try again later.</div>
+  );
 }
---
+++ client/src/features/project-list/components/ProjectList/useFetchProjectsPage.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { usePageNumber, useProjectSearchParams } from "@/hooks";
+import { IProjectsPage } from "@/types";
+import { requestProjectsPage } from "./requestProjectsPage";
+
+export function useFetchProjectsPage() {
+  const pageNumber = usePageNumber();
+  const projectSearchParams = useProjectSearchParams();
+  const [projectsPage, setProjectsPage] = useState<IProjectsPage | null>();
+  useEffect(() => {
+    const loadProjectPage = async () => {
+      const loadedPage = await requestProjectsPage(
+        pageNumber,
+        projectSearchParams
+      );
+
+      setProjectsPage(loadedPage);
+    };
+    loadProjectPage();
+  }, [pageNumber, projectSearchParams]);
+
+  return projectsPage;
+}
