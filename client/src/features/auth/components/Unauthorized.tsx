1c08324b0881b9e85d710c3cc27be67660b73e4a
steins0668 <archelausagaton@gmail.com>
2025-07-28 19:08:11 +0800

    feat(auth): add go back button to the unauthorized page

--- client/src/features/auth/components/Unauthorized.tsx
+++ client/src/features/auth/components/Unauthorized.tsx
@@ -1,3 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export function Unauthorized() {
-  return <div>You are not authorized to access this page.</div>;
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+
+  return (
+    <section>
+      <h1>Unauthorized</h1>
+      <br />
+      <p>You do not have access to the requested page.</p>
+      <div className="flex-fill">
+        <button onClick={goBack}>Go Back</button>
+      </div>
+    </section>
+  );
 }
