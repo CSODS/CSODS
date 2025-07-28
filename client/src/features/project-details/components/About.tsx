ed2e9412f189197dc01001b2f18e3bf9cf3c2529
steins0668 <archelausagaton@gmail.com>
2025-07-29 00:38:43 +0800

    refactor: improve export readability by using named exports

--- client/src/features/project-details/components/About.tsx
+++ client/src/features/project-details/components/About.tsx
@@ -1,22 +1,25 @@
 import { PROJECT_DESCRIPTION } from "@/constants/defaults";
 import { useProjectDetails } from "@/hooks";
-import '../styles/scrollableStyles.module.css';
+import "../styles/scrollableStyles.module.css";
 
 interface AboutProps {
-    aboutRef?: React.RefObject<HTMLDivElement | null>
+  aboutRef?: React.RefObject<HTMLDivElement | null>;
 }
 
-export default function About({ aboutRef }: AboutProps) {
-    const project = useProjectDetails();
+export function About({ aboutRef }: AboutProps) {
+  const project = useProjectDetails();
 
-    return (
-        <div ref={aboutRef} className="h-100 px-3 p-2 card card-frost-gradient-2 hover-shadow border border-0 rounded-2">
-            <h2 className="m-0 pb-1 p-0 border-bottom border-2 border-frost-midnight fw-bold text-start color-frost-midnight">
-                About
-            </h2>
-            <p className="m-0 pt-2 p-0 lh-sm text-start fs-p color-frost-midnight scrollbar-azure overflow-y-auto">
-                {PROJECT_DESCRIPTION}
-            </p>
-        </div>
-    )
+  return (
+    <div
+      ref={aboutRef}
+      className="h-100 px-3 p-2 card card-frost-gradient-2 hover-shadow border border-0 rounded-2"
+    >
+      <h2 className="m-0 pb-1 p-0 border-bottom border-2 border-frost-midnight fw-bold text-start color-frost-midnight">
+        About
+      </h2>
+      <p className="m-0 pt-2 p-0 lh-sm text-start fs-p color-frost-midnight scrollbar-azure overflow-y-auto">
+        {PROJECT_DESCRIPTION}
+      </p>
+    </div>
+  );
 }
