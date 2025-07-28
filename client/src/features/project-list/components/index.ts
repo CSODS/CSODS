6dae13c464f403cb62275c2e117d08e453b63aac
steins0668 <archelausagaton@gmail.com>
2025-07-28 21:40:53 +0800

    refactor: clean up ProjectList component structure and add index export

--- client/src/features/project-list/components/index.ts
+++ client/src/features/project-list/components/index.ts
@@ -1,15 +1,7 @@
 import Header from "./Header";
 import Paginator from "./Paginator";
-import ProjectCard from "./ProjectList/ProjectCard";
-import ProjectList from "./ProjectList/ProjectList";
+import { ProjectList } from "./ProjectList";
 import SearchBar from "./SearchBar";
 import UpButton from "./UpButton";
 
-export {
-    Header,
-    Paginator,
-    ProjectCard,
-    ProjectList,
-    SearchBar,
-    UpButton
-};
+export { Header, Paginator, ProjectList, SearchBar, UpButton };
