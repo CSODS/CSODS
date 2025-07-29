8347b587f18239ad79a383f0e2afbdba21ac1087
steins0668 <archelausagaton@gmail.com>
2025-07-29 21:10:56 +0800

    refactor(auth): use authClient for requestSignIn util

--- client/src/features/auth/components/SignIn/utils/request-sign-in.ts
+++ client/src/features/auth/components/SignIn/utils/request-sign-in.ts
@@ -1,17 +1,16 @@
 import { AUTH_ENDPOINTS } from "@/features/auth/constants";
+import { authClient } from "@/features/auth/utils";
 import { AuthAttemptResult, SignInRequest, SignInResponse } from "../types";
-import { csodsClient } from "@/utils";
 
 export async function requestSignIn(
   signInRequest: SignInRequest
 ): Promise<AuthAttemptResult> {
-  const { PATH, SIGN_IN } = AUTH_ENDPOINTS;
-  const endpoint = PATH + SIGN_IN;
+  const { SIGN_IN } = AUTH_ENDPOINTS;
 
   const jsonRequest = JSON.stringify(signInRequest);
 
-  const result: AuthAttemptResult = await csodsClient
-    .post<SignInResponse>(endpoint, jsonRequest, {
+  const result: AuthAttemptResult = await authClient
+    .post<SignInResponse>(SIGN_IN, jsonRequest, {
       headers: { "Content-Type": "application/json" },
       withCredentials: true,
     })
