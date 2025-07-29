b64aeeb36baeccfb42dc35e5c59ee26caacca29d
steins0668 <archelausagaton@gmail.com>
2025-07-29 21:03:04 +0800

    feat(auth): tune rate limiters for each auth route (#88)

--- server/src/features/auth/middleware/rate-limiter.middleware.ts
+++ server/src/features/auth/middleware/rate-limiter.middleware.ts
@@ -1,7 +1,38 @@
-import rateLimit from "express-rate-limit";
+import rateLimit, { Options } from "express-rate-limit";
 
-export const authRouteLimiter = rateLimit({
+const defaultOptions: Partial<Options> = {
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  statusCode: 429,
+};
+
+export const signInLimiter = rateLimit({
+  ...defaultOptions,
+  windowMs: 30 * 60 * 1000, // 30 minutes
+  limit: 5, // 5 requests per window
+  message: "Too many sign-in attempts. Please try again after 30 minutes.",
+});
+
+export const registerLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
+  limit: 5, // 5 requests per window
+  standardHeaders: "draft-8", // idk wtf this is.
+  legacyHeaders: false,
+  message: "Too many register attempts. Please try again after 1 hour.",
+  statusCode: 429,
+});
+
+export const refreshLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  limit: 10, // 10 requests per window
+  standardHeaders: "draft-8", // idk wtf this is.
+  legacyHeaders: false,
+  message: "Too many auth requests. Please try again after 1 hour.",
+  statusCode: 429,
+});
+
+export const signOutLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 5, // 5 requests per window
   standardHeaders: "draft-8", // idk wtf this is.
   legacyHeaders: false,
