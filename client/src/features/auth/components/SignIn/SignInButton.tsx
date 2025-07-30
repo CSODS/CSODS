00208e2188d7174f24c4e1fbce0b949d00507806
steins0668 <archelausagaton@gmail.com>
2025-07-30 21:38:06 +0800

    feat: combine RememberMe component with SignInButton component

--- client/src/features/auth/components/SignIn/SignInButton.tsx
+++ client/src/features/auth/components/SignIn/SignInButton.tsx
@@ -1,6 +1,9 @@
+import { RememberMe } from "./RememberMe";
+
 export function SignInButton() {
   return (
-    <div className="m-0 px-3 row">
+    <div className="m-0 px-3 row row-cols-1">
+      <RememberMe />
       <button className="col">Sign In</button>
     </div>
   );
