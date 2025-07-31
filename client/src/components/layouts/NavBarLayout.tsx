f8d2c67524426d8326be6f2dd6586aa004ab4b06
steins0668 <archelausagaton@gmail.com>
2025-07-31 11:11:22 +0800

    chore: remove unused import

--- client/src/components/layouts/NavBarLayout.tsx
+++ client/src/components/layouts/NavBarLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { ADDRESSES } from "@/constants";
 import { CollapseControls, NavBarControls } from "../shared";
 
