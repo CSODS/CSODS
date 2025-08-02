0c0d334f567ea2a300b2fb8b7f752b3bbb9f385e
steins0668 <archelausagaton@gmail.com>
2025-08-02 14:50:43 +0800

    feat: tweak usePrimitive hook value parser and onChange handler to handle boolean values and checkbox type elements (#104)

--- client/src/hooks/useInput/usePrimitive.ts
+++ client/src/hooks/useInput/usePrimitive.ts
@@ -10,14 +10,16 @@ export function usePrimitive<T>(key: string, initValue: T) {
   const [value, setValue, reset] = usePrimitiveReset<T>(key, initValue);
 
   // parses change event value as the same type as initValue.
-  const parseValue = (val: string) => {
+  const parseValue = (val: string | boolean) => {
     if (typeof initValue === "number") return Number(val) as T;
-    if (typeof initValue === "boolean") return (val === "true") as T;
     return val as T;
   };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setValue(parseValue(e.target.value));
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    setValue(parseValue(value));
+  };
 
   return [value, reset, onChange] as const;
 }
