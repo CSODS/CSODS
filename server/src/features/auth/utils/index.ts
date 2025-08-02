843d4111cf8fcaba82097f50f2f2ce6f9a1c6ed9
steins0668 <archelausagaton@gmail.com>
2025-08-03 00:06:47 +0800

    refactor: append .util to create-token and update-user-refresh-token for uniformity

--- server/src/features/auth/utils/index.ts
+++ server/src/features/auth/utils/index.ts
@@ -1,5 +1,5 @@
 export * from "./create-jwt.util";
 export * from "./create-payload.util";
-export * from "./create-tokens";
-export * from "./update-user-refresh-token";
+export * from "./create-tokens.util";
+export * from "./update-user-refresh-token.util";
 export * from "./verify-password.util";
