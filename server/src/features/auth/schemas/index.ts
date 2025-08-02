b37b337440ebb34e1002dc183f95571ca21141f6
steins0668 <archelausagaton@gmail.com>
2025-08-02 19:43:39 +0800

    feat: add refresh token payload schema

--- server/src/features/auth/schemas/index.ts
+++ server/src/features/auth/schemas/index.ts
@@ -1,3 +1,4 @@
-export * from "./login.schema";
-export * from "./register.schema";
 export * from "./access-token-payload.schema";
+export * from "./login.schema";
+export * from "./refresh-token-payload.schema";
+export * from "./register.schema";
