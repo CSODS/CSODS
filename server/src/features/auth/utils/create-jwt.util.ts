e85e7fda2d613cc07b4989331dda314eecd1e6ad
steins0668 <archelausagaton@gmail.com>
2025-08-03 00:03:18 +0800

    chore: remove deprecated createJwtDeprecated util

--- server/src/features/auth/utils/create-jwt.util.ts
+++ server/src/features/auth/utils/create-jwt.util.ts
@@ -2,42 +2,6 @@ import jwt from "jsonwebtoken";
 import { AUTH } from "@data";
 import { AccessTokenPayload, RefreshTokenPayload } from "../schemas";
 
-/**
- * @deprecated
- */
-type JwtOptionsDeprecated = {
-  tokenType: AUTH.TokenType;
-};
-
-/**
- * @deprecated Please use new {@link createJwt}
- * @public
- * @function createJwtDeprecated
- * @description A utility function that creates and returns a `JWT` from a provided `payload`
- * object and `options` parameter.
- * @param payload The payload. Follows the {@link AccessTokenPayload} type.
- * @param options.tokenType The token type. See {@link TokenType} for info on the allowed
- * values.
- * @returns The created `JWT`.
- */
-export function createJwtDeprecated(
-  payload: AccessTokenPayload,
-  options: JwtOptionsDeprecated
-): string {
-  const { tokenType } = options;
-  const { secret, signOptions } = AUTH.TOKEN_CONFIG_RECORD[tokenType];
-
-  if (!secret) {
-    throw new Error(
-      `${tokenType.toUpperCase()}_TOKEN_SECRET is not defined in env.`
-    );
-  }
-
-  const token: string = jwt.sign(payload, secret, signOptions);
-
-  return token;
-}
-
 type AccessOptions = {
   tokenType: Extract<AUTH.TokenType, "access">;
   payload: AccessTokenPayload;
