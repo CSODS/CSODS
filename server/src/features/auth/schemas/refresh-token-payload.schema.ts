bab8549409ee8c7fe7a014571af068e62b6810e0
steins0668 <archelausagaton@gmail.com>
2025-08-03 22:57:40 +0800

    feat: add isPersist field to refresh token payload schema

--- server/src/features/auth/schemas/refresh-token-payload.schema.ts
+++ server/src/features/auth/schemas/refresh-token-payload.schema.ts
@@ -4,4 +4,5 @@ export type RefreshTokenPayload = z.infer<typeof refreshTokenPayload>;
 
 export const refreshTokenPayload = z.object({
   userId: z.number(),
+  isPersist: z.boolean().optional(),
 });
