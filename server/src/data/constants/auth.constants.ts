61901e0582c1775d88cecd22f45b59f28513a645
steins0668 <archelausagaton@gmail.com>
2025-08-03 22:21:34 +0800

    feat: add `sessionCookie` config in TOKEN_CONFIG_RECORD

--- server/src/data/constants/auth.constants.ts
+++ server/src/data/constants/auth.constants.ts
@@ -32,6 +32,7 @@ type CookieConfig = {
   cookieName: string;
   clearCookie: CookieOptions;
   persistentCookie: CookieOptions;
+  sessionCookie: CookieOptions;
 };
 type TokenConfig = {
   secret: string | undefined;
@@ -69,6 +70,11 @@ export const TOKEN_CONFIG_RECORD: TokenConfigRecord = {
         sameSite: "none",
         secure: true,
         maxAge: 24 * 60 * 60 * 1000, // 1 day
+      },
+      sessionCookie: {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
       },
     },
   },
