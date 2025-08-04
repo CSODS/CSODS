0dfbec11413313ab251f74e2c1ed128fb709dc7a
steins0668 <archelausagaton@gmail.com>
2025-08-04 16:58:33 +0800

    feat(db): add refreshToken model

---
+++ server/src/models/refresh-token.ts
@@ -0,0 +1,30 @@
+import {
+  index,
+  integer,
+  sqliteTable,
+  text,
+  uniqueIndex,
+} from "drizzle-orm/sqlite-core";
+import { User } from "./user";
+import { isNull } from "drizzle-orm";
+
+export const RefreshToken = sqliteTable(
+  "refresh_tokens",
+  {
+    tokenId: integer("token_id").primaryKey(),
+    userId: integer("user_id")
+      .notNull()
+      .references(() => User.userId, { onDelete: "cascade" }),
+    refreshTokenHash: integer("refresh_token_hash").notNull(),
+    createdAt: text("created_at"),
+    expiresAt: text("expires_at"),
+    lastUsedAt: text("last_used_at"),
+  },
+  (table) => [
+    uniqueIndex("token_hash_idx").on(table.refreshTokenHash),
+    index("persistent_clean_up_idx").on(table.expiresAt),
+    index("session_clean_up_idx")
+      .on(table.expiresAt, table.lastUsedAt)
+      .where(isNull(table.expiresAt)),
+  ]
+);
