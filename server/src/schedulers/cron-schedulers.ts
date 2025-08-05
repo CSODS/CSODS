ffcd54f4c8d824b6b141bde19c1402895e23a54d
steins0668 <archelausagaton@gmail.com>
2025-08-05 23:53:20 +0800

    feat: add projectsJobs to `cronScheduler`

--- server/src/schedulers/cron-schedulers.ts
+++ server/src/schedulers/cron-schedulers.ts
@@ -1,6 +1,21 @@
 import cron from "node-cron";
-import { authJobs } from "@/features";
+import { authJobs, projectsJobs } from "@/features";
+
+const viewsDecayJobService = projectsJobs.createViewsDecayJobService();
+const evictionJobService = projectsJobs.createEvictionJobService();
 
 cron.schedule("0 0 0 * * *", authJobs.cleanUpIdleSessions);
 
-cron.schedule("0 0 0 * * *", authJobs.cleanUpExpiredSessions);
+cron.schedule("0 5 0 * * *", authJobs.cleanUpExpiredSessions);
+
+// every hour at 0 minutes
+viewsDecayJobService.scheduleViewsDecay();
+
+// every day
+evictionJobService.scheduleProjectCacheEviction();
+
+// every hour at 3 minutes
+evictionJobService.scheduleSearchCacheEviction();
+
+// every hour at 5 minutes
+evictionJobService.scheduleCachePageEviction();
