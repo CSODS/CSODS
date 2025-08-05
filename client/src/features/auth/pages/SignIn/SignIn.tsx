6623cccf4ca52469b38e6c3ca9d7383047653f60
Daina Parducho <dainajoyperolina@gmail.com>
2025-08-05 19:08:07 +0800

    feat(auth): restructure SignIn component layout and enhance styling

--- client/src/features/auth/pages/SignIn/SignIn.tsx
+++ client/src/features/auth/pages/SignIn/SignIn.tsx
@@ -5,28 +5,66 @@ import styles from "./SignIn.module.scss";
 
 export function SignIn() {
   const registerLink = ADDRESSES.AUTH.PATH + "/" + ADDRESSES.AUTH.REGISTER;
-  const loginContainer = styles["login-container"];
+  const { 
+    "login-container": loginContainer,
+    "login-wrapper": loginWrapper,
+    "login-left": loginLeft,
+    "login-right": loginRight,
+    "create-account-button": createAccount, 
+    } = styles;
 
   return (
-    <section
-      className={`mt-2 px-2 h-100 bg-default-dark-purple ${loginContainer}`}
-    >
-      {/* 1st half here */}
-      {/* 2nd half */}
-      <section className="">
-        <SignInForm />
-      </section>
-      <section className="mt-3 text-center">
-        <p className="m-0 p-0 color-light-2 small">
-          Don't have an account?{" "}
-          <NavLink
-            to={registerLink}
-            className="text-decoration-none"
+    <section className={`mt-2 px-2 bg-default-dark-purple ${loginContainer}`}>
+      <div className={`${loginWrapper}`}>
+
+        {/* 1st half */}
+        <div className={`${loginLeft}`}>
+          <img
+            src="/lucso-logo-no-bg.png"
+            alt="CSODS Logo"
+            className="img-fluid mb-3"
+            style={{ maxWidth: "150px" }}
+          />
+
+          <h1 className="bolder">
+            CSODS
+          </h1>
+
+          <p className="text-justify px-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Curabitur eu mollis leo, at commodo turpis. 
+            Quisque venenatis iaculis facilisis. 
+            Donec tempor eu sem ut tincidunt.
+          </p>
+          
+          <button
+            className={`${createAccount}`}
+            onClick={() => window.location.href = ADDRESSES.AUTH.REGISTER}
           >
-            Sign Up
-          </NavLink>
-        </p>
-      </section>
+            CREATE ACCOUNT
+          </button>
+
+        </div>
+
+        {/* 2nd half */}
+        <div className={`${loginRight}`}>
+          <div>
+            <SignInForm />
+            <section className="mt-3 text-center">
+              <p className="m-0 p-0 color-light-2 small">
+                Don't have an account?{" "}
+                <NavLink
+                  to={registerLink}
+                  className="text-decoration-none"
+                >
+                  Sign Up
+                </NavLink>
+              </p>
+            </section>
+          </div>
+        </div>
+
+      </div>    
     </section>
   );
 }
