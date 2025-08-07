f570bc4d49b3c3bb409ee97968db80699dcc36fc
steins0668 <archelausagaton@gmail.com>
2025-08-07 19:14:26 +0800

    feat: add new `ProjectDataService` class

---
+++ server/src/features/projects/services/project-data-service/get-cache-filename.ts
@@ -0,0 +1,40 @@
+import { CACHE } from "@/data";
+import { ProjectFilter } from "../repositories";
+import { HashService } from "@/utils";
+
+export function getCacheFilename(
+  filenameGenerator: (...nameElements: string[]) => string,
+  options?: {
+    isHardBackup?: boolean;
+    isToday?: boolean;
+    date?: Date;
+    filter?: ProjectFilter;
+    isFiltered?: boolean;
+  }
+): string {
+  //  Resolve options.
+  const isHardBackup: boolean = options?.isHardBackup ?? false;
+  const isToday: boolean = options?.isToday ?? false;
+  //  todo: replace with static class method implementations for better readability.
+  const filter: ProjectFilter | undefined = options?.filter;
+  const isFiltered: boolean = options?.isFiltered ?? false;
+
+  //  Assemble filename elements.
+  const { HARD_BACKUP, BASE_NAME } = CACHE.PROJECT_CACHE;
+  const baseName = isHardBackup ? HARD_BACKUP : BASE_NAME;
+
+  const date: Date | undefined = isToday ? new Date() : options?.date;
+  const dateString = date?.toISOString().split("T")[0] ?? "";
+
+  const filterList = filter?.getFilterList() ?? ["nofilter"];
+
+  const hasSearchKey = filter?.hasProjectSearchKey() ?? false;
+
+  filterList[0] = hasSearchKey
+    ? HashService.simpleHash(filterList[0]) //  hashed searchkey
+    : filterList[0];
+
+  const filterString = isFiltered ? filterList.join("_") : "nofilter";
+
+  return filenameGenerator(baseName, filterString, dateString);
+}
---
+++ server/src/features/projects/services/project-data-service/project-data.service.ts
@@ -0,0 +1,340 @@
+import { CACHE } from "@/data";
+import {
+  CachePageRecord,
+  IProjectCache,
+  IProjectCachePage,
+  IProjectDetails,
+} from "../../types";
+import { ProjectCachePageService } from "../cache";
+import { ProjectDbFetchService } from "../project-db-fetch.service";
+import { IProjectFilter, ProjectFilter } from "../repositories";
+import { getCacheFilename } from "./get-cache-filename";
+
+export class ProjectDataService {
+  private _projectCachePageService: ProjectCachePageService;
+  private _projectDbFetchService: ProjectDbFetchService;
+
+  public constructor(
+    projectCachePageService: ProjectCachePageService,
+    projectDbFetchService: ProjectDbFetchService
+  ) {
+    this._projectCachePageService = projectCachePageService;
+    this._projectDbFetchService = projectDbFetchService;
+  }
+
+  /**
+   * ! this method must be called after loading the cache into the memory.
+   * @param param0
+   */
+  public async getProjectByPageAndId({
+    pageNumber,
+    projectId,
+  }: {
+    pageNumber: number;
+    projectId: number;
+  }) {
+    //  get cache page
+    //  get project from cache page.
+  }
+
+  /**
+   * ! this method must be called after loading the cache into the memory.
+   * @description
+   * @param pageNumber
+   */
+  public async getOrCreatePage(pageNumber: number) {
+    try {
+      const cache = this._projectCachePageService.getCache();
+      //  ! throws TypeError if cache is null.
+      this._projectCachePageService.assertCacheNotNull(cache);
+
+      //  ? get page from cache
+      //  ? if out of bounds, return null
+      //  ? if missing page and cache is backup, return null
+      //  ? if missing page and cache isn't backup, load new page into cache.
+      //  ? update view count
+      //  ? return cache page.
+    } catch (err) {}
+  }
+
+  /**
+   * @description Asynchronously fetches a page from the database, loading it into
+   * the in memory cache.
+   * @param loadOptions
+   * @returns
+   */
+  public async loadNewCachePage(loadOptions: {
+    pageNumber: number;
+    filter?: IProjectFilter;
+  }): Promise<IProjectCachePage> {
+    const { pageNumber, filter } = loadOptions;
+    try {
+      const cache = this._projectCachePageService.getCache();
+      this._projectCachePageService.assertCacheNotNull(cache);
+
+      const pageSize = CACHE.PROJECT_CACHE.PAGE_SIZE;
+      const projects = (
+        await this._projectDbFetchService.fetchProjectsPages({
+          pageStart: pageNumber,
+          pageSize,
+          filter,
+        })
+      )[pageNumber];
+
+      const now = new Date();
+      const cachePage: IProjectCachePage = {
+        createdOn: now,
+        lastAccessed: now,
+        viewCount: 1,
+        totalPages: cache.totalPages,
+        projects,
+      };
+
+      const storedPage = await this._projectCachePageService.storeCachePage({
+        pageNumber,
+        cachePage,
+      });
+
+      return storedPage;
+    } catch (err) {
+      //  todo: handle errors
+      throw err;
+    }
+  }
+  /**
+   * @async
+   * @description Asynchronously retrieves data from a resolved cache whose key is
+   * generated from an {@link IProjectFilter} object. If both cache loading and
+   * creation fails, attempts to load data from a backup. The final value is
+   * then returned.
+   * @param filterOptions Optional filtering parameters used to narrow down cached
+   * projects.
+   * @returns A `Promise` that resolves to the loaded cache, or `null` if no results
+   * were loaded from an active filter, or all loading attempts failed.
+   */
+  public async getCache(
+    filterOptions?: IProjectFilter
+  ): Promise<IProjectCache | null> {
+    //  todo: replace with static class method implementation for better readability
+    let filter: ProjectFilter | undefined = new ProjectFilter(filterOptions);
+    filter = filter.isEmpty() ? undefined : filter;
+
+    const filename = getCacheFilename(
+      this._projectCachePageService.generateCacheFilename,
+      {
+        isToday: true,
+        filter,
+        isFiltered: !!filter,
+      }
+    );
+
+    this._projectCachePageService.setFilename(filename);
+    let cache = await this.tryResolveCache({ filter });
+
+    if (!cache) cache = await this.tryLoadBackupCache();
+
+    return cache;
+  }
+
+  /**
+   * @async
+   * @description Asynchronously attempts to load the cache into memory. If the
+   * operation fails, attempts to create a new cache for up to three times if
+   * needed.
+   * @param filter An object containing filter details for the database query.
+   * @returns A `Promise` that resolves to the loaded or newly created cache,
+   * or `null` if both load and creation operations failed.
+   */
+  public async tryResolveCache({
+    filter,
+  }: {
+    filter?: IProjectFilter;
+  }): Promise<IProjectCache | null> {
+    let cache: IProjectCache | null = await this.tryLoadCache();
+
+    if (cache) {
+      //  todo: log success
+      return cache;
+    } else {
+      //  todo: log failure
+      for (let i = 0; i < 3; i++) {
+        const { PAGE_SIZE } = CACHE.PROJECT_CACHE;
+        const createdCache: IProjectCache | null = await this.tryCreateCache({
+          currentDate: new Date(),
+          pageSize: PAGE_SIZE,
+          filter,
+        });
+
+        if (createdCache) return createdCache;
+      }
+    }
+
+    //  todo: log failure to parse existing cache and create new cache.
+    return null;
+  }
+
+  /**
+   * @async
+   * @description Wrapper for setCache method that returns `null` on failure.
+   * @returns
+   */
+  public async tryLoadCache(): Promise<IProjectCache | null> {
+    try {
+      //  !throws if cache is invalid.
+      return await this._projectCachePageService.loadCache();
+    } catch {
+      return null;
+    }
+  }
+
+  /**
+   * @description Asynchronously attempts to create a new cache by fetching data
+   * from the database, building a new cache, then persisting it to the cache
+   * storage with {@link ProjectCachePageService}.
+   * @param createOptions.currentDate The current date to denote when the cache
+   * is created.
+   * @param createOptions.pageSize Denotes how many projects will be stored in
+   * each page of the cache.
+   * @param createOptions.filter An optional {@link IProjectFilter} object for
+   * filtering the contents the database that will be stored in the cache.
+   * @returns A `Promise` that resolves to the {@link IProjectCache} or `null`
+   * if the cache creation fails.
+   */
+  public async tryCreateCache(createOptions: {
+    currentDate: Date;
+    pageSize: number;
+    filter?: IProjectFilter;
+  }): Promise<IProjectCache | null> {
+    try {
+      const { currentDate, pageSize, filter } = createOptions;
+      //  todo: add logging
+      //  !throws if there are no projects in db.
+      const { totalPages, pageRecord } = await this.fetchCacheData({
+        filter,
+        pageSize,
+      });
+
+      const newCache = this.buildProjectCache({
+        totalPages,
+        currentDate,
+        pageRecord,
+      });
+
+      //  !throws if constructed cache is invalid.
+      const storedCache = await this._projectCachePageService.persistCache(
+        newCache
+      );
+      return storedCache;
+    } catch (err) {
+      //  todo: log error
+      return null;
+    }
+  }
+  /**
+   * @description Asychronously attempts to load a backup cache.
+   * Utilizes the {@link tryLoadCache} after setting the cache path and filename
+   * to the backup cache's specifications.
+   * @returns A `Promise` that resolves to the loaded backup cache, or `null`
+   * if the cache path isn't configured, or the cache loading operation fails.
+   */
+  private async tryLoadBackupCache() {
+    //  todo: add logging
+    try {
+      const backupPath = process.env.DEFAULT_CACHE_PATH!;
+      if (!backupPath) throw new Error("Default cache path not configured.");
+
+      this._projectCachePageService.setCachePath(backupPath);
+
+      const filename = getCacheFilename(
+        this._projectCachePageService.generateCacheFilename,
+        { isHardBackup: true }
+      );
+      this._projectCachePageService.setFilename(filename);
+
+      const backupCache = await this.tryLoadCache();
+
+      if (!backupCache) {
+        //  todo: log failure to load backup cache.
+      }
+
+      return backupCache;
+    } catch (err) {
+      //  todo: log config error.
+      return null;
+    }
+  }
+
+  /**
+   * @description Asynchronously fetches data required for the cache from the
+   * database.
+   * @param fetchOptions.filter Optional filtering parameters used to narrow
+   * down the database query.
+   * @param fetchOptions.pageSize Specifies how many projects will be stored
+   * in each page.
+   * @returns A `Promise` that resolves to an object containing the totalPages
+   * of the cache and a `Record` containing the pages of the cache.
+   */
+  private async fetchCacheData(fetchOptions: {
+    filter?: IProjectFilter;
+    pageSize: number;
+  }) {
+    const { filter, pageSize } = fetchOptions;
+    const projectsCount = await this._projectDbFetchService.fetchProjectsCount(
+      filter
+    );
+
+    if (projectsCount === 0) throw new Error("Project list is empty.");
+    const totalPages = Math.ceil(projectsCount / pageSize);
+
+    const pageRecord = await this._projectDbFetchService.fetchProjectsPages({
+      pageStart: 1,
+      pageSize,
+      isAscending: false,
+      filter,
+    });
+
+    return { totalPages, pageRecord };
+  }
+
+  /**
+   * @description A utility method that builds a new {@link IProjectCache} object
+   * from the provided arguments.
+   * @param cacheData.totalPages The total pages of the paginated projects,
+   * database included
+   * @param cacheData.currentDate The date the cache is created.
+   * @param cacheData.isBackup Specifies if the project is a backup.
+   * @param cacheData.pageRecord A `Record` object containing each page of the
+   * cache.
+   * @returns A new {@link IProjectCache} instance.
+   */
+  private buildProjectCache(cacheData: {
+    totalPages: number;
+    currentDate: Date;
+    isBackup?: boolean;
+    pageRecord: Record<number, IProjectDetails[]>;
+  }): IProjectCache {
+    const { totalPages, currentDate, isBackup, pageRecord } = cacheData;
+
+    let cachePages: CachePageRecord = {};
+    const recordEntries = Object.entries(pageRecord);
+    recordEntries.forEach(([pageNumber, projects]) => {
+      const numericKey = Number(pageNumber);
+      cachePages[numericKey] = {
+        createdOn: currentDate,
+        lastAccessed: currentDate,
+        viewCount: 0,
+        totalPages,
+        projects,
+      };
+    });
+
+    return {
+      totalPages,
+      createdOn: currentDate,
+      lastAccessed: currentDate,
+      viewCount: 1,
+      isBackup: isBackup ?? false,
+      cachePages,
+    };
+  }
+}
