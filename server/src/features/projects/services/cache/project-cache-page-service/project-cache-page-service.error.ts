1bdd8cd2a81da542c8df68cbe54bc8e01315e23b
steins0668 <archelausagaton@gmail.com>
2025-08-08 00:22:54 +0800

    feat: add ProjectCachePageService csutom error class

---
+++ server/src/features/projects/services/cache/project-cache-page-service/project-cache-page-service.error.ts
@@ -0,0 +1,8 @@
+import { ErrorBase } from "@/error";
+
+export type ErrorName =
+  | "PAGE_OUT_OF_BOUNDS_ERROR"
+  | "MISSING_PAGE_ERROR"
+  | "PAGE_RETRIEVAL_ERROR";
+
+export class ProjectCachePageError extends ErrorBase<ErrorName> {}
