b7b9b857c0d128306fd4b4ec06a3364c32cdac63
steins0668 <archelausagaton@gmail.com>
2025-08-08 00:37:49 +0800

    chore: add projet-data-service to export barrel

--- server/src/features/projects/services/index.ts
+++ server/src/features/projects/services/index.ts
@@ -2,4 +2,5 @@ export * from "./cache";
 export * from "./eviction";
 export * from "./repositories";
 export * from "./legacy-project-data.service";
+export * from "./project-data-service";
 export * from "./projects-views-decay.service";
--- server/src/features/projects/services/project-data-service/index.ts
+++ server/src/features/projects/services/project-data-service/index.ts
@@ -1,1 +1,1 @@
-export * from "./project-data.service";
+export * from "./project-data-service";
