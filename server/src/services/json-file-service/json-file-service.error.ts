5722bfbabba914f71dc7becbb956010bde9b7c61
steins0668 <archelausagaton@gmail.com>
2025-08-07 20:38:12 +0800

    feat: add `JsonError` class for `JsonFileService`

---
+++ server/src/services/json-file-service/json-file-service.error.ts
@@ -0,0 +1,11 @@
+import { ErrorBase } from "@/error";
+
+type ErrorName =
+  | "JSON_FILE_NOT_FOUND_ERROR"
+  | "JSON_PARSE_ERROR"
+  | "JSON_WRITE_ERROR"
+  | "JSON_DELETE_ERROR"
+  | "INVALID_MODEL_ERROR"
+  | "NULL_DATA_ERROR";
+
+export class JsonError extends ErrorBase<ErrorName> {}
