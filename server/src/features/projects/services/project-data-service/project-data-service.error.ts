7cc7b2639bcc59e605aa97073218c71964574dff
steins0668 <archelausagaton@gmail.com>
2025-08-08 23:47:10 +0800

    feat: add RETRIEVE_PROJECTS_ERROR type to ProjectError

--- server/src/features/projects/services/project-data-service/project-data-service.error.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.error.ts
@@ -1,5 +1,5 @@
 import { ErrorBase } from "@/error";
 
-export type ErrorName = "FETCH_ERROR";
+export type ErrorName = "FETCH_ERROR" | "RETRIEVE_PROJECTS_ERROR";
 
 export class ProjectError extends ErrorBase<ErrorName> {}
