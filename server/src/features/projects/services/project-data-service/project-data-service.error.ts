b2ec21d911c6c6a162bac9aa8cf29f5735fd2504
steins0668 <archelausagaton@gmail.com>
2025-08-09 00:24:45 +0800

    feat: add LOAD_FROM_CACHE_ERROR type literal for ProjectError type

--- server/src/features/projects/services/project-data-service/project-data-service.error.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.error.ts
@@ -3,6 +3,7 @@ import { ErrorBase } from "@/error";
 export type ErrorName =
   | "FETCH_ERROR"
   | "RETRIEVE_PROJECTS_ERROR"
-  | "RESOLVE_PROJECTS_ERROR";
+  | "RESOLVE_PROJECTS_ERROR"
+  | "LOAD_FROM_CACHE_ERROR";
 
 export class ProjectError extends ErrorBase<ErrorName> {}
