532b2e1400c47e2a5e72f12b5d7dc4a69166d90e
steins0668 <archelausagaton@gmail.com>
2025-08-08 23:56:30 +0800

    feat: add RESOLVE_PROJECTS_ERROR literal  to type ErrorName

--- server/src/features/projects/services/project-data-service/project-data-service.error.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.error.ts
@@ -1,5 +1,8 @@
 import { ErrorBase } from "@/error";
 
-export type ErrorName = "FETCH_ERROR" | "RETRIEVE_PROJECTS_ERROR";
+export type ErrorName =
+  | "FETCH_ERROR"
+  | "RETRIEVE_PROJECTS_ERROR"
+  | "RESOLVE_PROJECTS_ERROR";
 
 export class ProjectError extends ErrorBase<ErrorName> {}
