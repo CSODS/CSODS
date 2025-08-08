ab898a56271c47704cb4cb1f3389e3a5c28c1ab6
steins0668 <archelausagaton@gmail.com>
2025-08-09 00:58:32 +0800

    feat: add normalizeProjectError function for normalizing error objects into the ProjectError class

--- server/src/features/projects/services/project-data-service/project-data-service.error.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.error.ts
@@ -9,3 +9,26 @@ export type ErrorName =
   | "CREATE_NEW_CACHE_ERROR";
 
 export class ProjectError extends ErrorBase<ErrorName> {}
+
+/**
+ * @description Normalizes err objects in catch blocks specifically for
+ * `ProjectDataService`.
+ * !DO NOT USE THIS FOR ANY OTHER CLASS.
+ * @param name The error name. See {@link ErrorName} for possible values.
+ * @param message A human readable error message.
+ * @param err The err argument in a catch block.
+ * @returns A new instance of the {@link ProjectError} class.
+ */
+export function normalizeProjectError<E extends ErrorName>(
+  name: E,
+  message: string,
+  err: unknown
+): ProjectError {
+  return err instanceof ProjectError
+    ? err
+    : new ProjectError({
+        name,
+        message,
+        cause: err,
+      });
+}
