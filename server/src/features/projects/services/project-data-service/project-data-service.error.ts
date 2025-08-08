04ca2aefcb413b7b069914b2ae16e86db037f8cb
steins0668 <archelausagaton@gmail.com>
2025-08-09 00:46:06 +0800

    feat: add multiple type literals for ProjectError denoting cache loading error, backup loading error, and new cache creation errror

--- server/src/features/projects/services/project-data-service/project-data-service.error.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.error.ts
@@ -4,6 +4,8 @@ export type ErrorName =
   | "FETCH_ERROR"
   | "RETRIEVE_PROJECTS_ERROR"
   | "RESOLVE_PROJECTS_ERROR"
-  | "LOAD_FROM_CACHE_ERROR";
+  | "LOAD_FROM_CACHE_ERROR"
+  | "LOAD_BACKUP_ERROR"
+  | "CREATE_NEW_CACHE_ERROR";
 
 export class ProjectError extends ErrorBase<ErrorName> {}
