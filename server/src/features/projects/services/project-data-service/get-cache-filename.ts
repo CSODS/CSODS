64f66767b547c750f702f52fa9ffc91e0cd8f9fa
steins0668 <archelausagaton@gmail.com>
2025-08-09 17:20:48 +0800

    feat: add getKey method to incrementally replace getCacheFilename

--- server/src/features/projects/services/project-data-service/get-cache-filename.ts
+++ server/src/features/projects/services/project-data-service/get-cache-filename.ts
@@ -44,3 +44,48 @@ export function getCacheFilename(
 
   return filenameGenerator(baseName, filterString, dateString);
 }
+
+/**
+ * todo: change filter param type to IProjectFilter
+ * @param filenameGenerator
+ * @param options
+ * @returns
+ */
+export function getKey(options?: {
+  isHardBackup?: boolean;
+  isToday?: boolean;
+  date?: Date;
+  filter?: ProjectFilter;
+  isFiltered?: boolean;
+}): string {
+  //  Resolve options.
+  const isHardBackup: boolean = options?.isHardBackup ?? false;
+  const isToday: boolean = options?.isToday ?? false;
+  //  todo: replace with static class method implementations for better readability.
+  const filter: ProjectFilter | undefined = options?.filter;
+  const isFiltered: boolean = options?.isFiltered ?? false;
+
+  //  Assemble filename elements.
+  const { HARD_BACKUP, BASE_NAME } = CACHE.PROJECT_CACHE;
+  const baseName = isHardBackup ? HARD_BACKUP : BASE_NAME;
+
+  const date: Date | undefined = isToday ? new Date() : options?.date;
+  const dateString = date?.toISOString().split("T")[0] ?? "";
+
+  const filterList = filter?.getFilterList() ?? ["nofilter"];
+
+  const hasSearchKey = filter?.hasProjectSearchKey() ?? false;
+
+  filterList[0] = hasSearchKey
+    ? HashService.simpleHash(filterList[0]) //  hashed searchkey
+    : filterList[0];
+
+  const filterString = isFiltered ? filterList.join("_") : "nofilter";
+
+  return generateKey(baseName, filterString, dateString);
+}
+
+function generateKey(...keyElements: string[]) {
+  const filtered = keyElements.filter(Boolean); // filter out falsy values.
+  return keyElements.join("-");
+}
