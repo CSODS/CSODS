dd492523659a79a6497a94303b7a234ab67b4c23
steins0668 <archelausagaton@gmail.com>
2025-08-09 20:15:26 +0800

    feat: add STORE_CACHE_PAGE_ERROR type literal to ProjectError

--- server/src/features/projects/services/project-data-service/project-data-service.error.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.error.ts
@@ -6,7 +6,8 @@ export type ErrorName =
   | "RESOLVE_PROJECTS_ERROR"
   | "LOAD_FROM_CACHE_ERROR"
   | "LOAD_BACKUP_ERROR"
-  | "CREATE_NEW_CACHE_ERROR";
+  | "CREATE_NEW_CACHE_ERROR"
+  | "STORE_CACHE_PAGE_ERROR";
 
 export class ProjectError extends ErrorBase<ErrorName> {}
 
