cb598e82a7f880a5e25adcc8d7886cd619b70713
steins0668 <archelausagaton@gmail.com>
2025-08-10 21:32:38 +0800

    feat: add BACKUP_CACHE type literal for ProjectResult Source

--- server/src/features/projects/services/project-data-service/project-data-service.type.ts
+++ server/src/features/projects/services/project-data-service/project-data-service.type.ts
@@ -2,7 +2,7 @@ import { ResultFail, ResultSuccess } from "@/types";
 import { IProjectCache, IProjectCachePage } from "../../types";
 import { ProjectError } from "./project-data-service.error";
 
-export type Source = "DATABASE" | "JSON_CACHE";
+export type Source = "DATABASE" | "JSON_CACHE" | "BACKUP_CACHE";
 export type ProjectResult =
   | ResultSuccess<IProjectCache, Source>
   | ResultFail<ProjectError>;
