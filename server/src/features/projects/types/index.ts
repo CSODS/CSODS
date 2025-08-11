09b58842f0cf278634d65faa695dcd9838bdd4bd
steins0668 <archelausagaton@gmail.com>
2025-08-11 21:01:49 +0800

    feat: add ProjectTables type

--- server/src/features/projects/types/index.ts
+++ server/src/features/projects/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./project-data-service.type";
 export * from "./project-filter.type";
+export * as Tables from "./project-tables.type";
 export * from "./project-tag-data-service.type";
 export * from "./project-tags-cache.interface";
 export * from "./project-viewmodels.type";
