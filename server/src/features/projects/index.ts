70d718779494251b1b08f1ff180154daeadd3d08
steins0668 <archelausagaton@gmail.com>
2025-08-11 22:40:27 +0800

    feat: add v2 api route constants to projects feature exports

--- server/src/features/projects/index.ts
+++ server/src/features/projects/index.ts
@@ -1,6 +1,8 @@
+export * as ProjectsApiV2 from "./constants/api-routes.constant";
 export * as projectsJobs from "./jobs";
 export * as projectsMiddlewares from "./middleware";
 export * as projectsServices from "./services";
 export * as projectsTypes from "./types";
 export * from "./project-tags.router";
+export * from "./projects-v2.routes";
 export * from "./projects.router";
