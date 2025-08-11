d138558561a4f8975a7f510d890b98af0e1c53a1
steins0668 <archelausagaton@gmail.com>
2025-08-11 21:51:51 +0800

    feat: add ProjectTagsDbFetcher service class

---
+++ server/src/features/projects/services/project-tags-db-fetcher.service.ts
@@ -0,0 +1,50 @@
+import * as Repositories from "./repositories";
+
+//  todo: document this
+export class ProjectTagsDbFetcher {
+  private readonly _applicationIndustryRepo: Repositories.ApplicationIndustryRepository;
+  private readonly _databaseTechnologyRepo: Repositories.DatabaseTechnologyRepository;
+  private readonly _devTypeRepo: Repositories.DevTypeRepository;
+  private readonly _frameworkRepo: Repositories.FrameworkRepository;
+  private readonly _programmingLanguageRepo: Repositories.ProgrammingLanguageRepository;
+
+  public constructor({
+    applicationIndustryRepo,
+    databaseTechnologyRepo,
+    devTypeRepo,
+    frameworkRepo,
+    programmingLanguageRepo,
+  }: {
+    applicationIndustryRepo: Repositories.ApplicationIndustryRepository;
+    databaseTechnologyRepo: Repositories.DatabaseTechnologyRepository;
+    devTypeRepo: Repositories.DevTypeRepository;
+    frameworkRepo: Repositories.FrameworkRepository;
+    programmingLanguageRepo: Repositories.ProgrammingLanguageRepository;
+  }) {
+    this._applicationIndustryRepo = applicationIndustryRepo;
+    this._databaseTechnologyRepo = databaseTechnologyRepo;
+    this._devTypeRepo = devTypeRepo;
+    this._frameworkRepo = frameworkRepo;
+    this._programmingLanguageRepo = programmingLanguageRepo;
+  }
+
+  public async fetchAllApplicationIndustries() {
+    return await this._applicationIndustryRepo.getAll();
+  }
+
+  public async fetchAllDatabaseTechnologies() {
+    return await this._databaseTechnologyRepo.getAll();
+  }
+
+  public async fetchAllDevTypes() {
+    return await this._devTypeRepo.getAll();
+  }
+
+  public async fetchAllFrameworks() {
+    return await this._frameworkRepo.getAll();
+  }
+
+  public async fetchAllProgrammingLanguages() {
+    return await this._programmingLanguageRepo.getAll();
+  }
+}
