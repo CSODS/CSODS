571df931a88f1be34557a7270075b4a8a48e14f0
steins0668 <archelausagaton@gmail.com>
2025-08-11 21:22:46 +0800

    feat: add ProjectTagCacheManager class

---
+++ server/src/features/projects/services/project-tag-data-service/project-tag-cache-manager.ts
@@ -0,0 +1,71 @@
+import { fail, success } from "@/utils";
+import { createProjectTagCacheService, ProjectTagCacheService } from "../cache";
+import { ProjectTagError } from "../../errors";
+import { IProjectTagsCache, ProjectTagsResult } from "../../types";
+import * as ViewModels from "../../types/project-viewmodels.type";
+
+export function createProjectTagCacheManager() {
+  const cacheService = createProjectTagCacheService();
+  return new ProjectTagCacheManager(cacheService);
+}
+
+//  todo: add docs
+export class ProjectTagCacheManager {
+  private readonly _cacheService: ProjectTagCacheService;
+
+  public constructor(projectTagCacheService: ProjectTagCacheService) {
+    this._cacheService = projectTagCacheService;
+  }
+
+  public async createAndStoreCache({
+    devTypes,
+    programmingLanguages,
+    frameworks,
+    databaseTechnologies,
+    applicationIndustries,
+  }: {
+    devTypes: ViewModels.DevTypeViewModel[];
+    programmingLanguages: ViewModels.ProgrammingLanguageViewModel[];
+    frameworks: ViewModels.FrameworkViewModel[];
+    databaseTechnologies: ViewModels.DatabaseTechnologyViewModel[];
+    applicationIndustries: ViewModels.ApplicationIndustryViewModel[];
+  }): Promise<ProjectTagsResult> {
+    const data: IProjectTagsCache = {
+      devTypes,
+      programmingLanguages,
+      frameworks,
+      databaseTechnologies,
+      applicationIndustries,
+      createdOn: new Date(),
+      lastAccessed: new Date(),
+      viewCount: 0,
+    };
+
+    try {
+      const storedCache = await this._cacheService.persistCache(data);
+      return success(storedCache);
+    } catch (err) {
+      const error = new ProjectTagError.ProjectTagError({
+        name: "CREATE_NEW_CACHE_ERROR",
+        message: "Error creating project tags cache.",
+        cause: err,
+      });
+
+      return fail(error);
+    }
+  }
+
+  public async loadCache(): Promise<ProjectTagsResult> {
+    try {
+      const tags = await this._cacheService.loadCache();
+      return success(tags, "JSON_CACHE");
+    } catch (err) {
+      const error = new ProjectTagError.ProjectTagError({
+        name: "LOAD_FROM_CACHE_ERROR",
+        message: "Error loading tags from cache.",
+        cause: err,
+      });
+      return fail(error);
+    }
+  }
+}
