0230b91f249d26a18ed899d7181b24aba9077334
steins0668 <archelausagaton@gmail.com>
2025-08-11 21:20:29 +0800

    refactor: rename TagResult to ProjectTagsResult

--- server/src/features/projects/types/project-tag-data-service.type.ts
+++ server/src/features/projects/types/project-tag-data-service.type.ts
@@ -3,6 +3,6 @@ import { ProjectTagError } from "../errors";
 import { DataSource } from "./data-source.type";
 import { IProjectTagsCache } from "./project-tags-cache.interface";
 
-export type TagResult =
+export type ProjectTagsResult =
   | ResultSuccess<IProjectTagsCache, DataSource>
   | ResultFail<ProjectTagError.ProjectTagError>;
