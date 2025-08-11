b5add487f09ea2714f81b5ee7e4e7d30634049ad
steins0668 <archelausagaton@gmail.com>
2025-08-11 13:54:51 +0800

    feat: add get project controller for projects feature

---
+++ server/src/features/projects/controllers/get-project.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { ProjectErrorStatusCodeMap as statusCodeMap } from "../constants";
+import type { IProjectDetails } from "../types";
+import { assembleFilter } from "../utils";
+import { StatusCode } from "@/utils";
+
+/**
+ * todo: update docs
+ *
+ * Result object follows shape of type {@link IProjectDetails}.
+ * @param req
+ * @param res
+ * @returns
+ */
+export async function getProject(req: Request, res: Response) {
+  const { page_number, project_id } = req.params;
+  const rawFilter = assembleFilter(req);
+
+  const dataService = req.projectDataService;
+  const projectResult = await dataService.getProjectByPageAndId({
+    pageNumber: Number(page_number),
+    projectId: Number(project_id),
+    rawFilter,
+  });
+
+  if (projectResult.success) return res.json(projectResult);
+
+  const { name: errorName } = projectResult.error;
+
+  return res
+    .status(StatusCode.fromError({ errorName, statusCodeMap }))
+    .json(projectResult);
+}
