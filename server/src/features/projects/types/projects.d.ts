22444dba32b9516e897ee0578d46303639fe5d65
steins0668 <archelausagaton@gmail.com>
2025-08-11 22:24:33 +0800

    feat: add ProjectTagsDataService to middleware

--- server/src/features/projects/types/projects.d.ts
+++ server/src/features/projects/types/projects.d.ts
@@ -1,9 +1,10 @@
-import { ProjectDataService } from "../services";
+import { ProjectDataService, ProjectTagsDataService } from "../services";
 
 declare global {
   namespace Express {
     interface Request {
       projectDataService: ProjectDataService;
+      projectTagsDataService: ProjectTagsDataService;
     }
   }
 }
