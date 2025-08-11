d2fe1a4cf1610416c05b9fa578fb1e7dffae6cb4
steins0668 <archelausagaton@gmail.com>
2025-08-11 22:54:13 +0800

    fix: projects feature v2 api routes

--- server/src/features/projects/constants/api-routes.constant.ts
+++ server/src/features/projects/constants/api-routes.constant.ts
@@ -1,8 +1,8 @@
 export const ROOT = "/projects-v2";
 
 export const ROUTES = {
-  BY_PAGE: "/:page_number",
-  BY_ID: "/:page_number/:project_id",
+  BY_PAGE: "/page/:page_number",
+  BY_ID: "/page/:page_number/project/:project_id",
   LOAD_PROJECTS: "/load-projects",
   ALL_TAGS: "/all-tags",
 } as const;
