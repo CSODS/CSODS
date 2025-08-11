6fc19e306578428b14586ec267a8c77402027e2b
steins0668 <archelausagaton@gmail.com>
2025-08-11 22:10:03 +0800

    feat: add resolveProjectTags method

--- server/src/features/projects/services/project-tags-data-service/project-tags-data-service.ts
+++ server/src/features/projects/services/project-tags-data-service/project-tags-data-service.ts
@@ -1,16 +1,42 @@
+import { fail } from "@/utils";
+import { ProjectTagError } from "../../errors";
+import { ProjectTagsResult } from "../../types";
+import { ProjectTagsDbFetcher } from "../project-tags-db-fetcher.service";
+import { fetchProjectTagsData } from "./fetch-project-tags-data";
 import { ProjectTagsCacheManager } from "./project-tags-cache-manager";
 
 export class ProjectTagsDataService {
   private readonly _cacheManager: ProjectTagsCacheManager;
+  private readonly _dbFetcher: ProjectTagsDbFetcher;
 
-  public constructor(projectTagCacheManager: ProjectTagsCacheManager) {
-    this._cacheManager = projectTagCacheManager;
+  public constructor(
+    projectTagsCacheManager: ProjectTagsCacheManager,
+    projectTagsDbFetcher: ProjectTagsDbFetcher
+  ) {
+    this._cacheManager = projectTagsCacheManager;
+    this._dbFetcher = projectTagsDbFetcher;
   }
 
-  public async resolveProjectTags() {
+  public async resolveProjectTags(): Promise<ProjectTagsResult> {
     const loadResult = await this._cacheManager.loadCache();
     if (loadResult.success) return loadResult;
 
-    const fetchResult = {};
+    const fetchResult = await fetchProjectTagsData(this._dbFetcher);
+    if (!fetchResult.success) return fetchResult; // fetch failed; return result
+
+    for (let i = 0; i < 3; i++) {
+      const createResult = await this._cacheManager.createAndStoreCache({
+        ...fetchResult.result,
+      });
+
+      if (createResult.success) return { ...createResult, source: "DATABASE" };
+    }
+
+    const error = new ProjectTagError.ProjectTagError({
+      name: "RESOLVE_TAGS_ERROR",
+      message: "Failed all attempts to crate tags cache.",
+    });
+
+    return fail(error);
   }
 }
