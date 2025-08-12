71f3bc0fe6da7ed41e45b4bd0321d5686a35dedc
steins0668 <archelausagaton@gmail.com>
2025-08-12 16:44:29 +0800

    feat: add `StatusCodeMap` namespace for projects feature

--- server/src/features/projects/constants/index.ts
+++ server/src/features/projects/constants/index.ts
@@ -1,3 +1,4 @@
 export * as API from "./api-routes.constant";
 export * from "./project-error-status-code-map";
 export * from "./project-tags-error-status-code-map";
+export * from "./status-code-map";
---
+++ server/src/features/projects/constants/status-code-map.ts
@@ -0,0 +1,29 @@
+import { StatusCodeMap as BaseMap } from "@/data";
+import { Exhaustive } from "@/types";
+import { Project, ProjectPage, ProjectTag } from "../errors";
+
+//  todo: add docs
+export namespace StatusCodeMap {
+  export const ProjectPageError: Exhaustive<ProjectPage.ErrorName> = {
+    PAGE_OUT_OF_BOUNDS_ERROR: 404,
+    MISSING_PAGE_ERROR: 404,
+    PAGE_RETRIEVAL_ERROR: 500,
+  } as const;
+
+  export const ProjectError: Exhaustive<Project.ErrorName> = {
+    ...BaseMap.CacheError,
+    ...BaseMap.DbError,
+    ...ProjectPageError,
+    RETRIEVE_PROJECTS_ERROR: 500,
+    RESOLVE_PROJECTS_ERROR: 500,
+    STORE_CACHE_PAGE_ERROR: 500,
+    MISSING_PROJECT_IN_PAGE_ERROR: 404,
+  } as const;
+
+  export const ProjectTagsError: Exhaustive<ProjectTag.ErrorName> = {
+    ...BaseMap.CacheError,
+    ...BaseMap.DbError,
+    RETRIEVE_TAGS_ERROR: 500,
+    RESOLVE_TAGS_ERROR: 500,
+  } as const;
+}
