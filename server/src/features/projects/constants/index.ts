3c69967a7d996c2843816c7308fb7be6448a1e08
steins0668 <archelausagaton@gmail.com>
2025-08-12 16:47:27 +0800

    chore: remove deprecated status code maps

--- server/src/features/projects/constants/index.ts
+++ server/src/features/projects/constants/index.ts
@@ -1,4 +1,2 @@
 export * as API from "./api-routes.constant";
-export * from "./project-error-status-code-map";
-export * from "./project-tags-error-status-code-map";
 export * from "./status-code-map";
