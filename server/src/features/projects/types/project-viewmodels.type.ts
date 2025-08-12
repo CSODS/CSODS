a5435221701ff11c9cfbae2e191c39ed25ec498b
steins0668 <archelausagaton@gmail.com>
2025-08-12 09:15:07 +0800

    refactor: remove tables types from project-viewmodels

--- server/src/features/projects/types/project-viewmodels.type.ts
+++ server/src/features/projects/types/project-viewmodels.type.ts
@@ -1,14 +1,5 @@
 import * as schema from "@models";
 
-export type ApplicationIndustryTable = typeof schema.ApplicationIndustry;
-export type DatabaseTechnologyTable = typeof schema.DatabaseTechnology;
-export type DevTypeTable = typeof schema.DevType;
-export type FrameworkTable = typeof schema.Framework;
-export type ProgrammingLanguageTable = typeof schema.ProgrammingLanguage;
-export type ProjectFrameworksTable = typeof schema.ProjectFramework;
-export type ProjectImagesTable = typeof schema.ProjectImage;
-export type ProjectsTable = typeof schema.Project;
-
 export type ApplicationIndustryViewModel =
   typeof schema.ApplicationIndustry.$inferSelect;
 export type DatabaseTechnologyViewModel =
