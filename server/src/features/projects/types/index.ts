a5de3c4531c1dd330ba77e967026dcc92a7688a2
steins0668 <archelausagaton@gmail.com>
2025-08-12 09:16:59 +0800

    feat: add namespace to project viewmodels exports

--- server/src/features/projects/types/index.ts
+++ server/src/features/projects/types/index.ts
@@ -3,5 +3,5 @@ export * from "./project-filter.type";
 export * as Tables from "./project-tables.type";
 export * from "./project-tag-data-service.type";
 export * from "./project-tags-cache.interface";
-export * from "./project-viewmodels.type";
+export * as ViewModels from "./project-viewmodels.type";
 export * from "./projects-cache.interface";
